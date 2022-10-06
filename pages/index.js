b317640031d34d4d8d2ab9c144191b210d1ac959
vinnidias <vinicius.3991@gmail.com>
2022-10-05 23:10:20 -0300

    corrigindo indentação

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,11 @@ const infoTabprops = [
   },
 ];
 
-const parceiros = [{ alt: "parceiros", src: "https://media.graphassets.com/ekDlCVRsRQuxlx6F5KUK" }, { alt: "parceiros", src: "https://media.graphassets.com/ekDlCVRsRQuxlx6F5KUK" }, { alt: "parceiros", src: "https://media.graphassets.com/ekDlCVRsRQuxlx6F5KUK" }]
+const parceiros = [
+  { alt: "parceiros", src: "https://media.graphassets.com/cCTNMOrTISFadTYxkD9x" },
+  { alt: "parceiros", src: "https://media.graphassets.com/IxM8ehfROeq8GxwVtwfA" },
+  { alt: "parceiros", src: "https://media.graphassets.com/ZMxaGH6zR86IjUgdLio5" }
+]
 
 
 export default function Home() {
@@ -41,32 +45,32 @@ export default function Home() {
         />
 
         <ImagemFundo
-          imagem = "https://www.impulsoprevine.org/imagem-fundo.png"
-          chamada = "Painéis de indicadores da plataforma"
-          chamadacolor = ""
-          subtexto = "A plataforma está dividida em três blocos que reúnem dados e indicadores para acompanhamento da RAPS."
-          cards = {[
-              {
-                  title : "Acompanhamento dos serviços CAPS",
-                  body : "Resumo, Perfil do Usuário, novos Usuários, taxa de Abandono, atendimentos individuais, Procedimento por usuário, Produção"
-              },
-              {
-                  title : "Outros serviços RAPS",
-                  body : "Resumo, Ambulatório de saúde mental, Consultório na rua, redução de danos"
-              },
-              {
-                  title : "Cuidado compartilhado de saúde mental",
-                  body : "Perfil do Usuário, Cuidado compartilhado entre APS e CAPS, Cuidado compartilhado entre APS e Ambulatório, Cuidado compartilhado de saúde mental com a rede de Urgência e emergência"
-              }
+          imagem="https://www.impulsoprevine.org/imagem-fundo.png"
+          chamada="Painéis de indicadores da plataforma"
+          chamadacolor=""
+          subtexto="A plataforma está dividida em três blocos que reúnem dados e indicadores para acompanhamento da RAPS."
+          cards={[
+            {
+              title: "Acompanhamento dos serviços CAPS",
+              body: "Resumo, Perfil do Usuário, novos Usuários, taxa de Abandono, atendimentos individuais, Procedimento por usuário, Produção"
+            },
+            {
+              title: "Outros serviços RAPS",
+              body: "Resumo, Ambulatório de saúde mental, Consultório na rua, redução de danos"
+            },
+            {
+              title: "Cuidado compartilhado de saúde mental",
+              body: "Perfil do Usuário, Cuidado compartilhado entre APS e CAPS, Cuidado compartilhado entre APS e Ambulatório, Cuidado compartilhado de saúde mental com a rede de Urgência e emergência"
+            }
           ]}
-          botao = {
-                      {
-                          label : "",
-                          url : ""
-                      }
+          botao={
+            {
+              label: "",
+              url: ""
+            }
           }
         />
-
+        
         <section id="sobre">
           <InfoTab contentList={infoTabprops} />
         </section>
