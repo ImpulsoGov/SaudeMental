73b097da9df4306eb050e6a6fca502757032db35
vinnidias <vinicius.3991@gmail.com>
2022-10-18 22:47:03 -0300

    query layout para app raiz

---
+++ querys/LAYOUT.js
@@ -0,0 +1,42 @@
+export const LAYOUT = `
+{
+  municipios {
+    nome
+    uf
+  }
+
+  logos {
+    logo {
+      url
+    }
+  }
+
+  menus {
+    label
+    url
+  }
+
+  submenus {
+    label
+    url
+  }
+
+  buttonImages {
+    image {
+      url
+    }
+  }
+
+  contactCopyrights {
+    email
+    copyright
+  }
+
+  socialMedias {
+    url
+    logo {
+      url
+    }
+  }
+}
+`
