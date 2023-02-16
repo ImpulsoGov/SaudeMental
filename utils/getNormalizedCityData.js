b7da6d9da2c67417f2cfac977f2d60190cde2a78
Taina Santana <tainnaps@gmail.com>
2023-02-16 14:22:35 -0300

    separa normalizacao da cidade em outro arquivo

---
+++ utils/getNormalizedCityData.js
@@ -0,0 +1,16 @@
+const COMBINING_CHARS_REGEX = /\p{Diacritic}/gu;
+const BLANK_SPACES_REGEX = /\s/g;
+
+export const getNormalizedCityData = (city) => {
+  const lowerCity = city.toLowerCase();
+  let [cityName, cityState] = lowerCity.split(' - ');
+
+  cityName = cityName.replace(BLANK_SPACES_REGEX, '-');
+
+  const normalizedCityName = cityName.normalize('NFD').replace(COMBINING_CHARS_REGEX, '');
+
+  return {
+    cityName: normalizedCityName,
+    cityState,
+  };
+};
