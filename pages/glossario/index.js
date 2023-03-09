241e7f731de17da656a7852928b1f80441a6d6ca
Gabrielle Arruda <gabriellearrudac@gmail.com>
2023-03-09 19:15:21 -0300

    Adiciona botao de voltar a glossario

--- pages/glossario/index.js
+++ pages/glossario/index.js
@@ -1,7 +1,9 @@
 import { ToggleList } from "@impulsogov/design-system";
 import { ToggleText } from "@impulsogov/design-system";
+import { ButtonLight } from "@impulsogov/design-system";
 import { GLOSSARIO } from "../../querys/GLOSSARIO";
 import { getData } from "../../services/getData";
+import style from "../duvidas/Duvidas.module.css"
 
 export async function getStaticProps() {
   const res = [
@@ -18,6 +20,9 @@ export async function getStaticProps() {
 export default function Glossario({res}) {
   return (
     <>
+      <div className={style.BotaoVoltar}>
+      <ButtonLight icone={{posicao: 'right',url: 'https://media.graphassets.com/8NbkQQkyRSiouNfFpLOG'}} label="VOLTAR" link="/inicio"/>
+      </div>
       <ToggleText 
         title={res[0].homeBanners[3].title} 
         list={res[0].toggleTexts} 
