251b31dda3848950b0ac777c763e622ef9bdf335
Taina Santana <tainnaps@gmail.com>
2023-03-18 03:31:54 -0300

    corrige imports e função de server side props

--- pages/duvidas/index.js
+++ pages/duvidas/index.js
@@ -1,12 +1,10 @@
-import { getSession } from "next-auth/react";
 import { ButtonLight } from "@impulsogov/design-system";
 import { TituloTexto } from "@impulsogov/design-system";
 import style from "./Duvidas.module.css"
-import { redirectHomeNotLooged } from "../../helpers/redirectHome";
+import { redirectHomeNotLooged } from "../../helpers/RedirectHome";
 
 export async function getServerSideProps(ctx) {
-  const session = await getSession(ctx)
-  const redirect = redirectHomeNotLooged(ctx,session)
+  const redirect = redirectHomeNotLooged(ctx)
   if(redirect) return redirect
   return { props: {} }
 }
@@ -23,17 +21,17 @@ const Index = () => {
   return (
     <>
       <div className={style.BotaoVoltar}>
-      <ButtonLight icone={{posicao: 'right',url: 'https://media.graphassets.com/8NbkQQkyRSiouNfFpLOG'}} label="VOLTAR" link="/inicio"/>
+        <ButtonLight icone={{posicao: 'right',url: 'https://media.graphassets.com/8NbkQQkyRSiouNfFpLOG'}} label="VOLTAR" link="/inicio"/>
       </div>
       <TituloTexto {...args} />
-      <div style={{marginTop: 0}} align="center" >
+      <div className={style.Iframe} align="center" >
         <iframe
           width="85%"
           height="2000"
           src="https://docs.google.com/forms/d/e/1FAIpQLSelCjrYy908a4dpluwiTI6uev78eDesDWKiHUixOheKzg1nhQ/viewform"
           allowFullScreen
-        /> 
-      </div>  
+        />
+      </div>
     </>
   )
 }
