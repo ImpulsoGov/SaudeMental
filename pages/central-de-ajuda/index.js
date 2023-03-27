700cd72aca275e93f690202b9a74fc00a03f4245
Taina Santana <tainnaps@gmail.com>
2023-03-27 10:48:33 -0300

    adiciona componente da central de ajuda

--- pages/central-de-ajuda/index.js
+++ pages/central-de-ajuda/index.js
@@ -1,26 +1,115 @@
-import { ButtonLight } from '@impulsogov/design-system'
-import { TituloTexto } from "@impulsogov/design-system";
-import style from "../duvidas/Duvidas.module.css"
+import { ButtonLight, CardLargeGridToggleList, TituloTexto } from '@impulsogov/design-system';
+import style from "../duvidas/Duvidas.module.css";
 
-const Index = ({res}) => {
-return(
-      <>
-      <div className={style.BotaoVoltar}>
-        <ButtonLight 
-            icone={{posicao: 'right', url: 'https://media.graphassets.com/8NbkQQkyRSiouNfFpLOG'}} 
-            label="VOLTAR" 
-            link="/inicio"/>
+const Index = ({ res }) => {
+  return (
+    <>
+      <div className={ style.BotaoVoltar }>
+        <ButtonLight
+          icone={ { posicao: 'right', url: 'https://media.graphassets.com/8NbkQQkyRSiouNfFpLOG' } }
+          label="VOLTAR"
+          link="/inicio"
+        />
       </div>
-      <TituloTexto 
-        imagem= {{
+      <TituloTexto
+        imagem={ {
           posicao: null,
           url: ''
-        }}
-        titulo= "Central de ajuda"
+        } }
+        titulo="Central de ajuda"
         texto=""
       />
-      </>
-  )
-}
+      <CardLargeGridToggleList
+        cards={ [
+          {
+            icon: 'https://media.graphassets.com/6cOfkxeyT7245Fn19kgU',
+            link: '/',
+            titulo: 'Sobre os indicadores'
+          },
+          {
+            icon: 'https://media.graphassets.com/6cOfkxeyT7245Fn19kgU',
+            link: '/',
+            titulo: 'Solicitar ajuda'
+          },
+          {
+            icon: 'https://media.graphassets.com/6cOfkxeyT7245Fn19kgU',
+            infos: [
+              {
+                content: 'email@email.com',
+                icon: 'https://media.graphassets.com/6cOfkxeyT7245Fn19kgU'
+              },
+              {
+                content: '(11) 91234-5678',
+                icon: 'https://media.graphassets.com/X8qAQFkrTLapqBDlfkMO'
+              }
+            ],
+            titulo: 'Contato'
+          }
+        ] }
+        theme="ColorSM"
+        togglelist={ {
+          icon: 'https://media.graphassets.com/Eu5DAy5SnCFbpRetwKSl',
+          list: [
+            {
+              blocks: [
+                {
+                  concept: {
+                    elements: [],
+                  },
+                  description: 'Como nossa intenção é conseguir levar o painel ao maior número de municípios possível, nesse momento não faremos nenhuma personalização. Os dados disponibilizados já são bastante completos, e já beneficiaram os outros municípios com os quais trabalhamos anteriormente. Caso tenham alguma sugestão de melhoria por favor nos envie, e avaliaremos a possibilidade de implementação',
+                  subTitle: 'Consigo personalizar o painel para o meu município?'
+                },
+                {
+                  concept: {
+                    elements: [],
+                  },
+                  description: 'Como nossa intenção é conseguir levar o painel ao maior número de municípios possível, nesse momento não faremos nenhuma personalização. Os dados disponibilizados já são bastante completos, e já beneficiaram os outros municípios com os quais trabalhamos anteriormente. Caso tenham alguma sugestão de melhoria por favor nos envie, e avaliaremos a possibilidade de implementação',
+                  subTitle: 'Qual é o objetivo da Impulso? Como o trabalho é financiado?'
+                },
+              ],
+              title: 'Interesses da Impulso e financiadores'
+            },
+            {
+              blocks: [
+                {
+                  description: 'Como nossa intenção é conseguir levar o painel ao maior número de municípios possível, nesse momento não faremos nenhuma personalização. Os dados disponibilizados já são bastante completos, e já beneficiaram os outros municípios com os quais trabalhamos anteriormente. Caso tenham alguma sugestão de melhoria por favor nos envie, e avaliaremos a possibilidade de implementação',
+                  subTitle: 'Para ter as informações do município no Painel é necessário que tenhamos prontuários e registros eletrônicos já implementados?'
+                },
+                {
+                  description: 'Como nossa intenção é conseguir levar o painel ao maior número de municípios possível, nesse momento não faremos nenhuma personalização.',
+                  subTitle: 'Os dados disponibilizados são apenas a partir do início da parceria ou existe dados históricos?'
+                },
+                {
+                  description: 'Como nossa intenção é conseguir levar o painel ao maior número de municípios possível, nesse momento não faremos nenhuma personalização.',
+                  subTitle: 'Os dados disponibilizados são apenas a partir do início da parceria ou existe dados históricos?'
+                },
+                {
+                  description: 'Como nossa intenção é conseguir levar o painel ao maior número de municípios possível, nesse momento não faremos nenhuma personalização.',
+                  subTitle: 'Como os dados são obtidos?'
+                }
+              ],
+              title: 'Dúvidas sobre a obtenção de dados'
+            },
+            {
+              blocks: [
+                {
+                  description: 'O documento possui validade de um ano.',
+                  subTitle: 'Quanto tempo dura a ACT?'
+                },
+                {
+                  description: 'Pré-selecionamos municípios que possuem, no mínimo, 3 CAPS + pelo menos um CAPS III ou IV.',
+                  subTitle: 'Como os municípios participantes são escolhidos?'
+                },
+              ],
+              title: 'Dúvidas administrativas'
+            }
+          ],
+          theme: 'LightGrey',
+          title: 'Perguntas frequentes'
+        } }
+      />
+    </>
+  );
+};
 
 export default Index;
