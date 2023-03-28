f76984ec6774d06aec68ea49f9ea296d0c695ba0
Gabrielle Arruda <gabriellearrudac@gmail.com>
2023-03-28 17:42:59 -0300

    Atualiza cuidado compartilhado

--- pages/cuidado-compartilhado/aps-caps/index.js
+++ pages/cuidado-compartilhado/aps-caps/index.js
@@ -1,4 +1,4 @@
-import { TituloTexto, ButtonLight, GraficoInfo, TituloSmallTexto } from "@impulsogov/design-system";
+import { TituloTexto, ButtonLight, GraficoInfo, TituloSmallTexto, Grid12Col, CardInfoTipoA } from "@impulsogov/design-system";
 import style from "../../duvidas/Duvidas.module.css";
 
 const ApsCaps = ({ }) => {
@@ -18,11 +18,60 @@ const ApsCaps = ({ }) => {
         descricao="Quantidade de matriciamentos separados pelos CAPS que estão dentro e fora da meta (meta: 2 matriciamentos / mês em cada CAPS)"
         fonte="Fonte: BPA/SIASUS - Elaboração Impulso Gov"
       />
+      <>
+          <Grid12Col
+            items={ [
+              <CardInfoTipoA
+                key={ 1345 }
+                indicador={ 0 }
+                titulo="CAPS fora da meta"
+              />,
+              <CardInfoTipoA
+                key={ 1347 }
+                indicador={ 0 }
+                titulo="CAPS dentro da meta"
+              />,
+              <CardInfoTipoA
+                key={ 1346 }
+                indicador={ 0 }
+                titulo="Total de matriciamentos (até Agosto)"
+              />,
+            ] }
+            proporcao="4-4-4"
+          />
+        </>
 
       <GraficoInfo
         titulo="Atendimentos"
         fonte="Fonte: SISAB - Elaboração Impulso Gov"
         descricao="Quantidade de atendimentos de Saúde Mental realizados pela APS que resultarem em Encaminhamentos para CAPS."
+      />
+        <>
+          <Grid12Col
+            items={ [
+              <CardInfoTipoA
+                key={ 1345 }
+                indicador={ 0 }
+                titulo="Total de atendimentos pela APS"
+              />,
+              <CardInfoTipoA
+                key={ 1347 }
+                indicador={ 0 }
+                titulo="Encaminhamentos para CAPS"
+              />,
+              <CardInfoTipoA
+                key={ 1346 }
+                indicador={ 0 }
+                indicadorSimbolo="%"
+                titulo="Porcentagem"
+              />,
+            ] }
+            proporcao="4-4-4"
+          />
+        </>
+
+      <GraficoInfo
+        descricao="<strong>Atenção:</strong> o número de atendimentos no gráfico a seguir está em escala logarítmica, que reforça as variações mês a mês quando os números são pequenos, e diminui a variação aparente quando os números são muito grandes."
       />
     </div>
   );
--- pages/cuidado-compartilhado/resumo/index.js
+++ pages/cuidado-compartilhado/resumo/index.js
@@ -1,4 +1,4 @@
-import { TituloTexto, ButtonLight, GraficoInfo, TituloSmallTexto } from "@impulsogov/design-system";
+import { TituloTexto, ButtonLight, GraficoInfo, TituloSmallTexto, Grid12Col, CardInfoTipoA, CardInfoTipoB } from "@impulsogov/design-system";
 import style from "../../duvidas/Duvidas.module.css";
 
 const Resumo = ({ }) => {
@@ -18,17 +18,39 @@ const Resumo = ({ }) => {
         fonte="Fonte: SISAB - Elaboração Impulso Gov"
         link={{label: 'Mais informações',url: '/cuidado-compartilhado/aps-caps'}}
       />
+      <Grid12Col
+        items={[
+          <CardInfoTipoB descricao="de 4823 atendimentos em saúde mental na APS" indicador={18} indicadorTotal={4823} indice={-4} indiceDescricao="últ. mês" titulo="Encaminhamentos para CAPS no mês de Junho" tooltip="Usuários que foram encaminhados para CAPS após atendimento em saúde mental ou abuso de substâncias pela Atenção Primária em Saúde"/>,
+          <CardInfoTipoA key={ 1345 } indicador={ 0 } titulo="CAPS fora da meta de matriciamento em 2022 (até Agosto)" tooltip="CAPS que realizaram menos de dois matriciamentos por mês no ano, até o mês de referência" />,
+        ]}
+      />
+      
 
       <GraficoInfo
         titulo="Cuidado compartilhado entre APS e Cuidado Ambulatorial"
         fonte="Fonte: SISAB - Elaboração Impulso Gov"
         link={{label: 'Mais informações',url: '/cuidado-compartilhado/aps-ambulatorio'}}
       />
+      <Grid12Col
+        items={[
+          <CardInfoTipoB descricao="de 6.111 atendimentos em saúde mental na APS" indicador={472} indicadorTotal={6111} 
+          indice={472} indiceDescricao="últ. mês" titulo="Encaminhamentos para cuidado ambulatorial no mês de Janeiro" 
+          tooltip="Usuários que foram encaminhados para cuidado ambulatorial (incluindo referências em psicologia e outros centros de especialidades) após atendimento em saúde mental ou abuso de substâncias pela Atenção Primária em Saúde"/>,
+        ]}
+      />
 
       <GraficoInfo
         titulo="Cuidado compartilhado entre RAPS e Rede de Urgência e Emergência"
         descricao="Fonte: RAAS/SIASUS, BPA/SIASUS, AIH/SIHSUS.- Elaboração Impulso Gov"
         link={{label: 'Mais informações',url: '/cuidado-compartilhado/raps-hospitalar'}}
+      />
+      <Grid12Col
+        items={[
+          <CardInfoTipoB descricao="de 151 atendimentos em saúde mental na APS" indicador={63} indicadorTotal={151} titulo="Atendidos na RAPS nos últimos 6 meses antes da Internação" 
+          tooltip="Usuários que tiveram ao menos um procedimento RAAS registrado em serviços RAPS dentro dos 6 meses anteriores a sua internação na rede hospitalar."/>,
+          <CardInfoTipoB descricao="de 3 atendimentos em saúde mental na APS" indicador={0} indicadorTotal={3} titulo="Atendidos na RAPS até o mês seguinte à alta" 
+          tooltip="Usuários que tiveram ao menos um procedimento RAAS registrado em serviços RAPS até o mês seguinte à alta de sua internação na rede hospitalar."/>
+        ]}
       />
     </div>
   );
