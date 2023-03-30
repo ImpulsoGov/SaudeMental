fcac63faa16e34e7a18c524c1bdba3f055c6f21f
Taina Santana <tainnaps@gmail.com>
2023-03-30 00:07:02 -0300

    add requisições de dados do painel aps caps

--- pages/cuidado-compartilhado/aps-caps/index.js
+++ pages/cuidado-compartilhado/aps-caps/index.js
@@ -1,7 +1,59 @@
-import { TituloTexto, ButtonLight, GraficoInfo, TituloSmallTexto, Grid12Col, CardInfoTipoA } from "@impulsogov/design-system";
-import style from "../../duvidas/Duvidas.module.css";
+import { CardInfoTipoA, GraficoInfo, Grid12Col, TituloSmallTexto } from "@impulsogov/design-system";
+import ReactEcharts from "echarts-for-react";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import { API_URL } from "../../../constants/API_URL";
+import { getEncaminhamentosChartOptions } from "../../../helpers/getEncaminhamentosChartOptions";
+import { redirectHomeNotLooged } from "../../../helpers/RedirectHome";
 
-const ApsCaps = ({ }) => {
+export function getServerSideProps(ctx) {
+  const redirect = redirectHomeNotLooged(ctx);
+
+  if (redirect) return redirect;
+
+  return { props: {} };
+}
+
+const ApsCaps = () => {
+  const { data: session } = useSession();
+  const [encaminhamentosApsCaps, setEncaminhamentosApsCaps] = useState([]);
+  const [encaminhamentosApsCapsResumo, setEncaminhamentosApsCapsResumo] = useState({});
+  const [matriciamentosPorMunicipio, setMatriciamentosPorMunicipio] = useState({});
+
+  useEffect(() => {
+    if (session?.user.municipio_id_ibge) {
+      const getRequestOptions = { method: 'GET', redirect: 'follow' };
+
+      const urlEncaminhamentosApsCaps = API_URL
+        + "saude-mental/encaminhamentos/aps/caps?municipio_id_sus="
+        + session?.user.municipio_id_ibge;
+
+      fetch(urlEncaminhamentosApsCaps, getRequestOptions)
+        .then(response => response.json())
+        .then(result => setEncaminhamentosApsCaps(result))
+        .catch(error => console.log('error', error));
+
+      const urlEncaminhamentosApsCapsResumo = API_URL
+        + "saude-mental/encaminhamentos/aps/caps/resumo?municipio_id_sus="
+        + session?.user.municipio_id_ibge;
+
+      fetch(urlEncaminhamentosApsCapsResumo, getRequestOptions)
+        .then(response => response.json())
+        .then(result => setEncaminhamentosApsCapsResumo(result[0]))
+        .catch(error => console.log('error', error));
+
+      const urlMatriciamentosPorMunicipio = API_URL
+        + "saude-mental/matriciamentos/municipio?municipio_id_sus="
+        + session?.user.municipio_id_ibge;
+
+      fetch(urlMatriciamentosPorMunicipio, getRequestOptions)
+        .then(response => response.json())
+        .then(result => setMatriciamentosPorMunicipio(result[0]))
+        .catch(error => console.log('error', error));
+    }
+  }, [session?.user.municipio_id_ibge]);
+
   return (
     <div>
       <TituloSmallTexto
@@ -18,50 +70,55 @@ const ApsCaps = ({ }) => {
         descricao="Quantidade de matriciamentos separados pelos CAPS que estão dentro e fora da meta (meta: 2 matriciamentos / mês em cada CAPS)"
         fonte="Fonte: BPA/SIASUS - Elaboração Impulso Gov"
       />
-      <>
+
+      { matriciamentosPorMunicipio &&
+        <>
           <Grid12Col
             items={ [
               <CardInfoTipoA
-                key={ 1345 }
-                indicador={ 0 }
+                key={ uuidv4() }
+                indicador={ matriciamentosPorMunicipio["estabelecimentos_fora_meta"] }
                 titulo="CAPS fora da meta"
               />,
               <CardInfoTipoA
-                key={ 1347 }
-                indicador={ 0 }
+                key={ uuidv4() }
+                indicador={ matriciamentosPorMunicipio["estabelecimentos_na_meta"] }
                 titulo="CAPS dentro da meta"
               />,
               <CardInfoTipoA
-                key={ 1346 }
-                indicador={ 0 }
-                titulo="Total de matriciamentos (até Agosto)"
+                key={ uuidv4() }
+                indicador={ matriciamentosPorMunicipio["quantidade_registrada"] }
+                titulo={ `Total de matriciamentos (até ${matriciamentosPorMunicipio["ate_mes"]})` }
               />,
             ] }
             proporcao="4-4-4"
           />
         </>
+      }
 
       <GraficoInfo
         titulo="Atendimentos"
         fonte="Fonte: SISAB - Elaboração Impulso Gov"
         descricao="Quantidade de atendimentos de Saúde Mental realizados pela APS que resultarem em Encaminhamentos para CAPS."
       />
+
+      { encaminhamentosApsCapsResumo &&
         <>
           <Grid12Col
             items={ [
               <CardInfoTipoA
-                key={ 1345 }
-                indicador={ 0 }
+                key={ uuidv4() }
+                indicador={ encaminhamentosApsCapsResumo["atendimentos_sm_aps"] }
                 titulo="Total de atendimentos pela APS"
               />,
               <CardInfoTipoA
-                key={ 1347 }
-                indicador={ 0 }
+                key={ uuidv4() }
+                indicador={ encaminhamentosApsCapsResumo["encaminhamentos_caps"] }
                 titulo="Encaminhamentos para CAPS"
               />,
               <CardInfoTipoA
-                key={ 1346 }
-                indicador={ 0 }
+                key={ uuidv4() }
+                indicador={ encaminhamentosApsCapsResumo["perc_encaminhamentos_caps"] }
                 indicadorSimbolo="%"
                 titulo="Porcentagem"
               />,
@@ -69,10 +126,17 @@ const ApsCaps = ({ }) => {
             proporcao="4-4-4"
           />
         </>
+      }
 
       <GraficoInfo
         descricao="<strong>Atenção:</strong> o número de atendimentos no gráfico a seguir está em escala logarítmica, que reforça as variações mês a mês quando os números são pequenos, e diminui a variação aparente quando os números são muito grandes."
       />
+      { encaminhamentosApsCaps.length !== 0 &&
+        <ReactEcharts
+          option={ getEncaminhamentosChartOptions(encaminhamentosApsCaps) }
+          style={ { width: "100%", height: "70vh" } }
+        />
+      }
     </div>
   );
 };
