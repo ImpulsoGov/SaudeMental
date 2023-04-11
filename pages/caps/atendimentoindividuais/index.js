77453b709dab1148c6cd1c69cda0b48064b831ff
Taina Santana <tainnaps@gmail.com>
2023-04-10 23:53:19 -0300

    add cards de atendimentos por estabelecimento

--- pages/caps/atendimentoindividuais/index.js
+++ pages/caps/atendimentoindividuais/index.js
@@ -1,8 +1,9 @@
-import { GraficoInfo, TituloSmallTexto } from "@impulsogov/design-system";
+import { CardInfoTipoA, GraficoInfo, Grid12Col, TituloSmallTexto } from "@impulsogov/design-system";
 import { useSession } from "next-auth/react";
-// import { useEffect, useState } from "react";
-// import { API_URL } from "../../../constants/API_URL";
+import { useEffect, useState } from "react";
+import { v1 as uuidv1 } from "uuid";
 import { redirectHomeNotLooged } from "../../../helpers/RedirectHome";
+import { getAtendimentosPorCaps, getPerfilDeAtendimentos, getResumoPerfilDeAtendimentos } from "../../../requests/caps";
 
 export function getServerSideProps(ctx) {
   const redirect = redirectHomeNotLooged(ctx);
@@ -14,6 +15,98 @@ export function getServerSideProps(ctx) {
 
 const AtendimentoIndividual = () => {
   const { data: session } = useSession();
+  const [perfilAtendimentos, setPerfilAtendimentos] = useState([]);
+  const [resumoPerfilAtendimentos, setResumoPerfilAtendimentos] = useState();
+  const [atendimentosPorCaps, setAtendimentosPorCaps] = useState([]);
+
+  useEffect(() => {
+    const getDados = async (municipioIdSus) => {
+      setPerfilAtendimentos(await getPerfilDeAtendimentos(municipioIdSus));
+      setResumoPerfilAtendimentos(
+        await getResumoPerfilDeAtendimentos(municipioIdSus)[0]
+      );
+      setAtendimentosPorCaps(await getAtendimentosPorCaps(municipioIdSus));
+    };
+
+    if (session?.user.municipio_id_ibge) {
+      getDados(session?.user.municipio_id_ibge);
+    }
+  }, []);
+
+  const agregarPorEstabelecimentoLinha = (atendimentos) => {
+    const atendimentosAgregados = [];
+
+    atendimentos.forEach((atendimento) => {
+      const {
+        estabelecimento,
+        nome_mes: nomeMes,
+        estabelecimento_linha_perfil: estabelecimentoLinha,
+        perc_apenas_atendimentos_individuais: porcentagemAtendimentos,
+        dif_perc_apenas_atendimentos_individuais: difPorcentagemAtendimentosAnterior
+      } = atendimento;
+
+      const estabelecimentoLinhaEncontrada = atendimentosAgregados
+        .find((item) => item.estabelecimentoLinha === estabelecimentoLinha);
+
+      if (!estabelecimentoLinhaEncontrada) {
+        atendimentosAgregados.push({
+          nomeMes,
+          estabelecimentoLinha,
+          atendimentosPorEstabelecimento: [{
+            estabelecimento,
+            porcentagemAtendimentos,
+            difPorcentagemAtendimentosAnterior
+          }]
+        });
+      } else {
+        estabelecimentoLinhaEncontrada.atendimentosPorEstabelecimento.push({
+          estabelecimento,
+          porcentagemAtendimentos,
+          difPorcentagemAtendimentosAnterior
+        });
+      }
+    });
+
+    return atendimentosAgregados;
+  };
+
+  const getCardsAtendimentosPorCaps = (atendimentos) => {
+    const atendimentosPorCapsUltimoPeriodo = atendimentos
+      .filter(({ periodo, estabelecimento }) => periodo === "Último período" && estabelecimento !== "Todos");
+
+    const atendimentosAgregados = agregarPorEstabelecimentoLinha(atendimentosPorCapsUltimoPeriodo);
+
+    const cardsAtendimentosPorCaps = atendimentosAgregados.map(({
+      estabelecimentoLinha, atendimentosPorEstabelecimento, nomeMes
+    }) => (
+      <>
+        <GraficoInfo
+          titulo={ `CAPS ${estabelecimentoLinha}` }
+          descricao={ `Dados de ${nomeMes}` }
+        />
+
+        <Grid12Col
+          items={
+            atendimentosPorEstabelecimento.map((item) => (
+              <CardInfoTipoA
+                titulo={ item.estabelecimento }
+                indicador={ item.porcentagemAtendimentos }
+                indicadorSimbolo="%"
+                indice={ item.difPorcentagemAtendimentosAnterior }
+                indiceSimbolo="p.p."
+                indiceDescricao="últ. mês"
+                key={ uuidv1() }
+              />
+            ))
+          }
+          proporcao="3-3-3-3"
+        />
+      </>
+    ));
+
+    return cardsAtendimentosPorCaps;
+  };
+
   return (
     <div>
       <TituloSmallTexto
@@ -22,18 +115,18 @@ const AtendimentoIndividual = () => {
           url: ''
         } }
         texto=""
-        titulo="<strong>Referências de Saúde Mental</strong>"
+        titulo="<strong>Atendimentos individuais </strong>"
       />
 
       <GraficoInfo
-        titulo="Referência de Saúde Mental"
-        fonte="Fonte: BPA/SIASUS - Elaboração Impulso Gov"
+        descricao="Taxa dos usuários frequentantes no mês que realizaram apenas atendimentos individuais"
+        fonte="Fonte: BPA-i e RAAS/SIASUS - Elaboração Impulso Gov"
       />
 
-      <GraficoInfo
-        titulo="Atendimentos"
-        fonte="Fonte: BPA/SIASUS - Elaboração Impulso Gov"
-      />
+      {
+        atendimentosPorCaps.length !== 0
+        && getCardsAtendimentosPorCaps(atendimentosPorCaps)
+      }
 
       <GraficoInfo
         titulo="Total de atendimentos"
