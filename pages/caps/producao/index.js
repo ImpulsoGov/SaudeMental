217af0b3ff7baf4629093fec4f88cde9a7edea17
Taina Santana <tainnaps@gmail.com>
2023-04-11 13:37:52 -0300

    add cards produção por hora

--- pages/caps/producao/index.js
+++ pages/caps/producao/index.js
@@ -1,8 +1,9 @@
-import { GraficoInfo, TituloSmallTexto } from "@impulsogov/design-system";
+import { CardInfoTipoA, GraficoInfo, Grid12Col, TituloSmallTexto } from "@impulsogov/design-system";
 import { useSession } from "next-auth/react";
-// import { useEffect, useState } from "react";
-// import { API_URL } from "../../../constants/API_URL";
+import { useEffect, useState } from "react";
+import { v1 as uuidv1 } from "uuid";
 import { redirectHomeNotLooged } from "../../../helpers/RedirectHome";
+import { getProcedimentosPorHora } from "../../../requests/caps";
 
 export function getServerSideProps(ctx) {
   const redirect = redirectHomeNotLooged(ctx);
@@ -14,6 +15,96 @@ export function getServerSideProps(ctx) {
 
 const Producao = () => {
   const { data: session } = useSession();
+  const [procedimentosPorHora, setProcedimentosPorHora] = useState([]);
+  // const [filtroEstabelecimentoHistorico, setFiltroEstabelecimentoHistorico] = useState({
+  //   value: "Todos", label: "Todos"
+  // });
+
+  useEffect(() => {
+    const getDados = async (municipioIdSus) => {
+      setProcedimentosPorHora(await getProcedimentosPorHora(municipioIdSus));
+    };
+
+    if (session?.user.municipio_id_ibge) {
+      getDados(session?.user.municipio_id_ibge);
+    }
+  }, []);
+
+  const agregarPorLinhaPerfil = (procedimentos) => {
+    const procedimentosAgregados = [];
+
+    procedimentos.forEach((procedimento) => {
+      const {
+        estabelecimento,
+        nome_mes: nomeMes,
+        estabelecimento_linha_perfil: linhaPerfil,
+        procedimentos_por_hora: procedimentosPorHora,
+        perc_dif_procedimentos_por_hora_anterior: porcentagemDifProcedimentosPorHoraAnterior
+      } = procedimento;
+
+      const linhaPerfilEncontrada = procedimentosAgregados
+        .find((item) => item.linhaPerfil === linhaPerfil);
+
+      if (!linhaPerfilEncontrada) {
+        procedimentosAgregados.push({
+          nomeMes,
+          linhaPerfil,
+          procedimentosPorEstabelecimento: [{
+            estabelecimento,
+            procedimentosPorHora,
+            porcentagemDifProcedimentosPorHoraAnterior
+          }]
+        });
+      } else {
+        linhaPerfilEncontrada.procedimentosPorEstabelecimento.push({
+          estabelecimento,
+          procedimentosPorHora,
+          porcentagemDifProcedimentosPorHoraAnterior
+        });
+      }
+    });
+
+    return procedimentosAgregados;
+  };
+
+  const getCardsProcedimentosHoraPorEstabelecimento = (procedimentos) => {
+    const procedimentosPorEstabelecimentoUltimoPeriodo = procedimentos
+      .filter(({ periodo, estabelecimento }) => periodo === "Último período" && estabelecimento !== "Todos");
+
+    const procedimentosAgregados = agregarPorLinhaPerfil(procedimentosPorEstabelecimentoUltimoPeriodo);
+
+    const cardsProcedimentosHoraPorEstabelecimento = procedimentosAgregados.map(({
+      linhaPerfil, procedimentosPorEstabelecimento, nomeMes
+    }) => (
+      <>
+        <GraficoInfo
+          titulo={ `CAPS ${linhaPerfil}` }
+          descricao="Comparativo de produção por hora de trabalho dos profissionais nos CAPS"
+          fonte={ `Dados de ${nomeMes}` }
+        />
+
+        <Grid12Col
+          items={
+            procedimentosPorEstabelecimento.map((item) => (
+              <CardInfoTipoA
+                titulo={ item.estabelecimento }
+                indicador={ item.procedimentosPorHora }
+                indicarDescricao="procedimentos/hora"
+                indice={ item.porcentagemDifProcedimentosPorHoraAnterior }
+                indiceSimbolo="%"
+                indiceDescricao="últ. mês"
+                key={ uuidv1() }
+              />
+            ))
+          }
+          proporcao="3-3-3-3"
+        />
+      </>
+    ));
+
+    return cardsProcedimentosHoraPorEstabelecimento;
+  };
+
   return (
     <div>
       <TituloSmallTexto
@@ -22,13 +113,17 @@ const Producao = () => {
           url: ''
         } }
         texto=""
-        titulo="<strong>Referências de Saúde Mental</strong>"
+        titulo="<strong>Produção</strong>"
       />
 
       <GraficoInfo
-        titulo="Referência de Saúde Mental"
-        fonte="Fonte: BPA/SIASUS - Elaboração Impulso Gov"
+        fonte="Fonte: BPA-c, BPA-i e RAAS/SIASUS - Elaboração Impulso Gov"
       />
+
+      {
+        procedimentosPorHora.length !== 0
+        && getCardsProcedimentosHoraPorEstabelecimento(procedimentosPorHora)
+      }
 
       <GraficoInfo
         titulo="Atendimentos"
