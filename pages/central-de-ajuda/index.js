ae4b7a002dc32f39defd75a7cfdae3a6a5975fd2
Gabrielle Arruda <gabriellearrudac@gmail.com>
2023-04-11 11:52:09 -0300

    Atualiza materiais da central de ajuda

--- pages/central-de-ajuda/index.js
+++ pages/central-de-ajuda/index.js
@@ -30,12 +30,12 @@ const Index = ({ res }) => {
         cards={ [
           {
             icon: 'https://media.graphassets.com/6cOfkxeyT7245Fn19kgU',
-            link: '/',
+            link: '/materiais-complementares',
             titulo: 'Sobre os indicadores'
           },
           {
             icon: 'https://media.graphassets.com/6cOfkxeyT7245Fn19kgU',
-            link: '/',
+            link: '/duvidas',
             titulo: 'Solicitar ajuda'
           },
           {
@@ -73,29 +73,41 @@ const Index = ({ res }) => {
               title: 'Dúvidas sobre o painel',
               blocks: [
                 {
-                  description: 'Como nossa intenção é conseguir levar o painel ao maior número de municípios possível, nesse momento não faremos nenhuma personalização. Os dados disponibilizados já são bastante completos, e já beneficiaram os outros municípios com os quais trabalhamos anteriormente. Caso tenham alguma sugestão de melhoria por favor nos envie, e avaliaremos a possibilidade de implementação',
-                  subTitle: 'Para ter as informações do município no Painel é necessário que tenhamos prontuários e registros eletrônicos já implementados?'
+                  subTitle: 'Consigo personalizar o painel para o meu município?',
+                  description: 'Nossa intenção é expandir o acesso ao painel e assim apoiar o maior número de municípios possível. Por isso, neste momento, não faremos personalizações na ferramenta.<br><br>Mas sabemos que, com o uso do painel, podem surgir ideias e necessidades de melhorias e adoraríamos receber suas sugestões. Vamos avaliar cada pedido com atenção e cuidado e avisaremos se a implementação for viável.',
                 },
                 {
-                  description: 'Como nossa intenção é conseguir levar o painel ao maior número de municípios possível, nesse momento não faremos nenhuma personalização.',
-                  subTitle: 'Os dados disponibilizados são apenas a partir do início da parceria ou existe dados históricos?'
+                  subTitle: 'Quais são os pré-requisitos técnicos que precisamos cumprir para que os dados do meu municípios sejam disponibilizados no painel?',
+                  description: 'Não é necessário ter nenhum tipo de prontuário ou digitalização de registro do dia-a-dia das unidades para usar o nosso painel. Os dados de CAPS são obtidos a partir das fichas RAAS e BPA que são mensalmente enviados diretamente para o Ministério da Saúde pelas unidades de saúde, secretarias de saúde ou prefeituras (o fluxo pode variar dependendo do seu município). Portanto, contanto que alguém esteja inserindo mensalmente os dados de RAAS e BPA no sistema do Ministério da Saúde, no fluxo que já é padrão para os municípios, sejam os prontuários físicos ou digitais, a ImpulsoGov terá acesso a todos os dados necessários para que você possa ver as informações sobre o seu município no painel de indicadores.<br></br>É importante destacar que, para garantir um melhor funcionando do painel de indicadores para o seu município, os registros devem ser preferencialmente feitos em fichas RAAS, com exceção do acolhimento inicial que parte das fichas de BPA individualizado (BPA-i), sendo poucos os procedimentos que deveriam ser prioritariamente registrados em BPA consolidado (BPA-c).<br></br>Chamamos atenção a esse ponto porque em casos de registros feitos em BPA-c, ao invés de RAAS ou BPA-i, os dados do seu município podem parecer subnotificados, uma vez que o registro consolidado impede o acesso à individualização dos atendimentos que a RAAS e o BPA-i permitem, não sendo possível, por exemplo, contabilizar se determinado paciente está tendo uma oferta adequada de cuidado multiprofissional, ou se não aderiu ao serviço após primeiro acolhimento.',
                 },
                 {
-                  description: 'Como nossa intenção é conseguir levar o painel ao maior número de municípios possível, nesse momento não faremos nenhuma personalização.',
-                  subTitle: 'Os dados disponibilizados são apenas a partir do início da parceria ou existe dados históricos?'
+                  subTitle: 'Os dados disponibilizados são apenas a partir do início da parceria ou existe dados históricos?',
+                  description: 'No painel de indicadores, baseado nas experiências que tivemos com municípios parceiros até o momento, mostramos dados históricos a partir de 2017, esse histórico não é influenciado pela data de início da parceria com a Impulso. Apesar disso, existe a possibilidade de mostrarmos períodos anteriores; caso o seu município queira consultar séries históricas prévias a 2017, orientamos que entre em contato com a nossa equipe para avaliação.',
                 },
                 {
-                  description: 'Como nossa intenção é conseguir levar o painel ao maior número de municípios possível, nesse momento não faremos nenhuma personalização.',
-                  subTitle: 'Como os dados são obtidos?'
-                }
+                  subTitle: 'Como os dados são obtidos?',
+                  description: 'Os dados são obtidos mensalmente a partir dos repositórios públicos do DataSUS. Esses repositórios contêm dados enviados pelos municípios após a remoção das informações pessoais que identificam os usuários e chegam para a ImpulsoGov criptografados. Buscamos estes dados nos repositórios de forma automatizada mensalmente e aplicamos uma série de tratamentos para calcular os indicadores exibidos no painel. <br><br>A parceria com a ImpulsoGov não exige nenhuma mudança na prática de registro e envio de dados para o Ministério da Saúde que o seu município já adota. Basta seguir fazendo os registros e envios mensais para que os dados sejam disponibilizados para a ImpulsoGov. É importante ressaltar que o Ministério da Saúde leva dois meses para disponibilizar os dados nos repositórios abertos. ',
+                },
+                {
+                  subTitle: 'Quando os dados são atualizados?',
+                  description: 'Os dados do painel são atualizados nas primeiras semanas de todo mês, a partir da disponibilização das informações do Ministério da Saúde nos repositórios públicos do DataSUS. Essa disponibilização do ministério ocorre com um atraso de dois meses em relação a competência atual. Por exemplo, se o mês vigente é março, mostraremos dados de janeiro no painel.',
+                },
+                {
+                  subTitle: 'Algo na tela não está carregando! O que fazer?',
+                  description: 'Indicamos uma lista de passos que você deve realizar, nesta ordem, caso algo não esteja carregando na tela:<ol><li>Verifique se sua conexão com a internet está ativa e estável;</li><li>Atualize a página pelo botão de recarregar página no seu navegador, ou utilizando a tecla F5 do teclado;</li><li>Limpe os dados de navegação e realize o login novamente;</li><li>Caso os passos anteriores não solucionem o problema de carregamento, entre em contato com a equipe pelo formulário de suporte.</li></ol>'                
+                },
+                {
+                  subTitle: 'Algum dado parece incorreto ou alguma informação dos CAPS está desatualizada! Como proceder?',
+                  description: '<b>Para valores que parecem incorretos</b><br><br>Considerando que os dados utilizados nos indicadores são extraídos diretamente dos repositórios do Ministério da Saúde, caso algum indicador pareça incorreto, primeiro é necessário verificar se a incongruência é relacionada à subnotificação de fichas. Isso pode ocorrer principalmente em três momentos:<ul><li> Forma de registro</li><li> Os registros estão sendo realizados corretamente, de forma completa, nos estabelecimentos de saúde?</li><li> Envio dos registros</li><li> Esses registros estão sendo enviados mensalmente para o Ministério da Saúde?</li><li> Correção de inconsistências</li><li> Caso o Ministério da Saúde identifique algum problema em algum registro, existe uma pessoa responsável por analisar o relatório de inconsistências e reenviar os dados?</li></ul>Após checagem dos pontos acima, caso a resposta seja *Sim* para as três perguntas, entre em contato com a nossa equipe pelo formulário de suporte relatando detalhadamente qual o indicador que parece estar incorreto e em quanto difere da realidade [caso saiba valores de parâmetro].<br><br><b>Para classificação de linha de cuidado de CAPS incorreta ou CAPS não aparecendo</b><br><br>Para a listagem dos CAPS ativos e classificação de linha de cuidado nossa equipe considerou as respostas enviadas por um gestor do município no formulário <em>Informações para personalização do painel</em>, preenchido no início da parceria com a ImpulsoGov. Caso algum CAPS apareça com a linha de cuidado incorreta (por exemplo, é Álcool e Drogas e está classificado como Geral), entre em contato com a nossa equipe pelo formulário de suporte indicando qual o nome do estabelecimento, como ele está sendo exibido no painel e como deveria ser mostrado.<br>Caso algum CAPS não esteja aparecendo como um estabelecimento no painel de indicadores, entre em contato com a nossa equipe pelo formulário de suporte indicando as informações abaixo:<br><blockquote>CNES <em>[insira aqui o CNES]</em>, nome oficial: <em>[insira aqui nome oficial]</em>, apelido: <em>[insira aqui o apelido se houver]</em>, Linha de cuidado: <em>[indique se Geral ou AD]</em>, Faixa etária de cuidado: <em>[indique se adulto ou inf/juv]</em></blockquote>'
+                },
               ]
             },
             {
               title: 'Dúvidas administrativas',
               blocks: [
                 {
-                  description: 'Quanto tempo dura a ACT? ',
-                  subTitle: 'O documento possui validade de um ano.'
+                  subTitle: 'Quanto tempo dura a ACT? ',
+                  description: 'O documento possui validade de um ano.'
                 },
                 {
                   description: 'Pré-selecionamos municípios que possuem, no mínimo, 3 CAPS + pelo menos um CAPS III ou IV.',
