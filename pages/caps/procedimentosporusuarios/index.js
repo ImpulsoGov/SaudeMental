e5def2ded8a15705a4e0b7597e13bbde48f3e4bc
Taina Santana <tainnaps@gmail.com>
2023-04-11 11:18:41 -0300

    add cards procedimentos por estabelecimento

--- pages/caps/procedimentosporusuarios/index.js
+++ pages/caps/procedimentosporusuarios/index.js
@@ -1,8 +1,9 @@
-import { GraficoInfo, TituloSmallTexto } from "@impulsogov/design-system";
+import { CardInfoTipoA, GraficoInfo, Grid12Col, TituloSmallTexto } from "@impulsogov/design-system";
 import { useSession } from "next-auth/react";
-// import { useEffect, useState } from "react";
-// import { API_URL } from "../../../constants/API_URL";
+import { useEffect, useState } from "react";
+import { v1 as uuidv1 } from "uuid";
 import { redirectHomeNotLooged } from "../../../helpers/RedirectHome";
+import { getProcedimentosPorEstabelecimento } from "../../../requests/caps";
 
 export function getServerSideProps(ctx) {
   const redirect = redirectHomeNotLooged(ctx);
@@ -14,6 +15,100 @@ export function getServerSideProps(ctx) {
 
 const ProcedimentosPorUsuarios = () => {
   const { data: session } = useSession();
+  const [procedimentosPorEstabelecimento, setProcedimentosPorEstabelecimento] = useState([]);
+  const [filtroEstabelecimentoHistorico, setFiltroEstabelecimentoHistorico] = useState({
+    value: "Todos", label: "Todos"
+  });
+
+  useEffect(() => {
+    const getDados = async (municipioIdSus) => {
+      // setPerfilAtendimentos(await getPerfilDeAtendimentos(municipioIdSus));
+      // setResumoPerfilAtendimentos(
+      //   await getResumoPerfilDeAtendimentos(municipioIdSus)[0]
+      // );
+      setProcedimentosPorEstabelecimento(
+        await getProcedimentosPorEstabelecimento(municipioIdSus)
+      );
+    };
+
+    if (session?.user.municipio_id_ibge) {
+      getDados(session?.user.municipio_id_ibge);
+    }
+  }, []);
+
+  const agregarPorLinhaPerfil = (procedimentos) => {
+    const procedimentosAgregados = [];
+
+    procedimentos.forEach((procedimento) => {
+      const {
+        estabelecimento,
+        nome_mes: nomeMes,
+        estabelecimento_linha_perfil: linhaPerfil,
+        procedimentos_por_usuario: procedimentosPorUsuario,
+        dif_procedimentos_por_usuario_anterior_perc: difPorcentagemProcedimentosAnterior
+      } = procedimento;
+
+      const linhaPerfilEncontrada = procedimentosAgregados
+        .find((item) => item.linhaPerfil === linhaPerfil);
+
+      if (!linhaPerfilEncontrada) {
+        procedimentosAgregados.push({
+          nomeMes,
+          linhaPerfil,
+          procedimentosPorEstabelecimento: [{
+            estabelecimento,
+            procedimentosPorUsuario,
+            difPorcentagemProcedimentosAnterior
+          }]
+        });
+      } else {
+        linhaPerfilEncontrada.procedimentosPorEstabelecimento.push({
+          estabelecimento,
+          procedimentosPorUsuario,
+          difPorcentagemProcedimentosAnterior
+        });
+      }
+    });
+
+    return procedimentosAgregados;
+  };
+
+  const getCardsProcedimentosPorEstabelecimento = (procedimentos) => {
+    const procedimentosPorEstabelecimentoUltimoPeriodo = procedimentos
+      .filter(({ periodo, estabelecimento }) => periodo === "Último período" && estabelecimento !== "Todos");
+
+    const procedimentosAgregados = agregarPorLinhaPerfil(procedimentosPorEstabelecimentoUltimoPeriodo);
+
+    const cardsProcedimentosPorEstabelecimento = procedimentosAgregados.map(({
+      linhaPerfil, procedimentosPorEstabelecimento, nomeMes
+    }) => (
+      <>
+        <GraficoInfo
+          titulo={ `CAPS ${linhaPerfil}` }
+          descricao={ `Dados de ${nomeMes}` }
+        />
+
+        <Grid12Col
+          items={
+            procedimentosPorEstabelecimento.map((item) => (
+              <CardInfoTipoA
+                titulo={ item.estabelecimento }
+                indicador={ item.procedimentosPorUsuario }
+                indice={ item.difPorcentagemProcedimentosAnterior }
+                indiceSimbolo="%"
+                indiceDescricao="últ. mês"
+                key={ uuidv1() }
+              />
+            ))
+          }
+          proporcao="3-3-3-3"
+        />
+      </>
+    ));
+
+    return cardsProcedimentosPorEstabelecimento;
+  };
+
   return (
     <div>
       <TituloSmallTexto
@@ -22,13 +117,18 @@ const ProcedimentosPorUsuarios = () => {
           url: ''
         } }
         texto=""
-        titulo="<strong>Referências de Saúde Mental</strong>"
+        titulo="<strong>Procedimentos por usuários</strong>"
       />
 
       <GraficoInfo
-        titulo="Referência de Saúde Mental"
-        fonte="Fonte: BPA/SIASUS - Elaboração Impulso Gov"
+        descricao="Taxa de procedimentos registrados pelo número de usuários com fichas movimentadas durante o mês de referência"
+        fonte="Fonte: BPA-i e RAAS/SIASUS - Elaboração Impulso Gov"
       />
+
+      {
+        procedimentosPorEstabelecimento.length !== 0
+        && getCardsProcedimentosPorEstabelecimento(procedimentosPorEstabelecimento)
+      }
 
       <GraficoInfo
         titulo="Atendimentos"
