93dfde8c1a6a05c63bee19f2b1292f5d428591db
Taina Santana <tainnaps@gmail.com>
2023-04-12 12:30:23 -0300

    add cards de usuários resumo caps

--- pages/caps/resumo/index.js
+++ pages/caps/resumo/index.js
@@ -1,8 +1,23 @@
-import { GraficoInfo, TituloSmallTexto } from "@impulsogov/design-system";
+import { CardInfoTipoA, GraficoInfo, Grid12Col, TituloSmallTexto } from "@impulsogov/design-system";
 import { useSession } from "next-auth/react";
-// import { useEffect, useState } from "react";
-// import { API_URL } from "../../../constants/API_URL";
+import { useState } from "react";
+import { v1 as uuidv1 } from "uuid";
 import { redirectHomeNotLooged } from "../../../helpers/RedirectHome";
+// import {
+//   getPerfilUsuariosPorEstabelecimento,
+//   getResumoNovosUsuarios,
+//   getAbandonoCoortes,
+//   getAtendimentosPorCaps,
+//   getProcedimentosPorEstabelecimento,
+//   getResumoProcedimentosPorTempoServico,
+//   getProcedimentosPorHora
+// } from "../../../requests/caps";
+import atendPorCaps from "../atendimentoindividuais/porCaps.json";
+import novosResumo from "../novosusuarios/novosResumo.json";
+import perfPorEstabelecimento from "../perfildousuario/perfilPorEstabelecimento.json";
+import procePorEstabelecimento from "../procedimentosporusuarios/porEstabelecimento.json";
+import procePorTempoServicoResumo from "../procedimentosporusuarios/porTempoServicoResumo.json";
+import procePorHora from "../producao/porHora.json";
 
 export function getServerSideProps(ctx) {
   const redirect = redirectHomeNotLooged(ctx);
@@ -14,6 +29,106 @@ export function getServerSideProps(ctx) {
 
 const Resumo = () => {
   const { data: session } = useSession();
+  const [procedimentosPorHora, setProcedimentosPorHora] = useState(procePorHora);
+  const [procedimentoPorTempoServicoResumo, setProcedimentosPorTempoServicoResumo] = useState(procePorTempoServicoResumo[0]);
+  const [procedimentosPorEstabelecimento, setProcedimentosPorEstabelecimento] = useState(procePorEstabelecimento);
+  const [atendimentoPorCaps, setAtendimentoPorCaps] = useState(atendPorCaps);
+  const [novosUsuariosResumo, setNovosUsuariosResumo] = useState(novosResumo);
+  const [perfilPorEstabelecimento, setPerfilPorEstabelecimento] = useState(perfPorEstabelecimento);
+  const [filtroEstabelecimento, setFiltroEstabelecimento] = useState({
+    value: "Todos", label: "Todos"
+  });
+
+  // useEffect(() => {
+  //   const getDados = async (municipioIdSus) => {
+  //     setProcedimentosPorHora(await getProcedimentosPorHora(municipioIdSus));
+  //     setProcedimentosPorTempoServicoResumo(await getResumoProcedimentosPorTempoServico(municipioIdSus)[0]);
+  //     setProcedimentosPorEstabelecimento(await getProcedimentosPorEstabelecimento(municipioIdSus));
+  //     setAtendimentoPorCaps(await getAtendimentosPorCaps(municipioIdSus));
+  //     setNovosUsuariosResumo(await getResumoNovosUsuarios(municipioIdSus));
+  //     setPerfilPorEstabelecimento(await getPerfilUsuariosPorEstabelecimento(municipioIdSus));
+  //   };
+
+  //   if (session?.user.municipio_id_ibge) {
+  //     getDados(session?.user.municipio_id_ibge);
+  //   }
+  // }, []);
+
+  const getDadosUltimoPeriodo = (lista) => {
+    return lista.filter((item) => item.periodo === "Último período");
+  };
+
+  const getDadosDeEstabelecimentoFiltrado = (lista, filtro) => {
+    return lista.find((item) => item.estabelecimento === filtro);
+  };
+
+  const getCardsResumoUsuarios = (perfilUsuarios, novosUsuarios, filtroEstabelecimento) => {
+    const perfilUsuariosUltimoPeriodo = getDadosUltimoPeriodo(perfilUsuarios);
+    const novosUsuariosUltimoPeriodo = getDadosUltimoPeriodo(novosUsuarios);
+    const perfilUsuariosDeEstabelecimento = getDadosDeEstabelecimentoFiltrado(perfilUsuariosUltimoPeriodo, filtroEstabelecimento);
+    const novosUsuariosDeEstabelecimento = getDadosDeEstabelecimentoFiltrado(novosUsuariosUltimoPeriodo, filtroEstabelecimento);
+
+    return (
+      <>
+        <GraficoInfo
+          descricao={ `Dados de ${perfilUsuariosDeEstabelecimento.nome_mes}` }
+        />
+
+        <Grid12Col
+          proporcao="4-4-4"
+          items={ [
+            <>
+              {
+                <CardInfoTipoA
+                  key={ uuidv1() }
+                  fonte="Fonte: BPA-i e RAAS/SIASUS - Elaboração Impulso Gov"
+                  indicador={ perfilUsuariosDeEstabelecimento.ativos_mes }
+                  indice={ perfilUsuariosDeEstabelecimento.dif_ativos_mes_anterior }
+                  indiceDescricao="últ. mês"
+                  link={ {
+                    label: 'Mais Informações',
+                    url: '/'
+                  } }
+                  titulo="Usuários que frequentaram"
+                  tooltip="a"
+                />
+              }
+            </>,
+            <>
+              {
+                <CardInfoTipoA
+                  key={ uuidv1() }
+                  fonte="Fonte: BPA-i e RAAS/SIASUS - Elaboração Impulso Gov"
+                  indicador={ perfilUsuariosDeEstabelecimento.tornandose_inativos }
+                  indice={ perfilUsuariosDeEstabelecimento.dif_tornandose_inativos_anterior }
+                  indiceDescricao="últ. mês"
+                  titulo="Tornaram-se inativos"
+                  tooltip="a"
+                />
+              }
+            </>,
+            <>
+              {
+                <CardInfoTipoA
+                  key={ uuidv1() }
+                  fonte="Fonte: RAAS/SIASUS - Elaboração Impulso Gov"
+                  indicador={ novosUsuariosDeEstabelecimento.usuarios_novos }
+                  indice={ novosUsuariosDeEstabelecimento.dif_usuarios_novos_anterior }
+                  indiceDescricao="últ. mês"
+                  link={ {
+                    label: 'Mais Informações',
+                    url: '/'
+                  } }
+                  titulo="Novos usuários"
+                  tooltip="a"
+                />
+              }
+            </>,
+          ] }
+        />
+      </>
+    );
+  };
   return (
     <div>
       <TituloSmallTexto
@@ -24,6 +139,16 @@ const Resumo = () => {
         texto=""
         titulo="<strong>Resumo</strong>"
       />
+
+      {
+        novosUsuariosResumo.length !== 0 &&
+        perfilPorEstabelecimento.length !== 0 &&
+        getCardsResumoUsuarios(
+          perfilPorEstabelecimento,
+          novosUsuariosResumo,
+          filtroEstabelecimento.value
+        )
+      }
 
       <GraficoInfo
         titulo="Taxa de abandono"
