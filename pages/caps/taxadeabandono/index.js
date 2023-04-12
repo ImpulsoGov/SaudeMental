1736da6628b257e306ea38330a940ba62c92d2af
Taina Santana <tainnaps@gmail.com>
2023-04-12 15:01:12 -0300

    add cards abandono acumulado

--- pages/caps/taxadeabandono/index.js
+++ pages/caps/taxadeabandono/index.js
@@ -1,8 +1,9 @@
-import { GraficoInfo, TituloSmallTexto } from "@impulsogov/design-system";
+import { CardInfoTipoA, GraficoInfo, Grid12Col, TituloSmallTexto } from "@impulsogov/design-system";
 import { useSession } from "next-auth/react";
-// import { useEffect, useState } from "react";
-// import { API_URL } from "../../../constants/API_URL";
+import { useEffect, useState } from "react";
+import { v1 as uuidv1 } from "uuid";
 import { redirectHomeNotLooged } from "../../../helpers/RedirectHome";
+import { getAbandonoCoortes } from "../../../requests/caps";
 
 export function getServerSideProps(ctx) {
   const redirect = redirectHomeNotLooged(ctx);
@@ -14,6 +15,47 @@ export function getServerSideProps(ctx) {
 
 const TaxaAbandono = () => {
   const { data: session } = useSession();
+  const [abandonoCoortes, setAbandonoCoortes] = useState([]);
+
+  useEffect(() => {
+    const getDados = async (municipioIdSus) => {
+      setAbandonoCoortes(await getAbandonoCoortes(municipioIdSus));
+    };
+
+    if (session?.user.municipio_id_ibge) {
+      getDados(session?.user.municipio_id_ibge);
+    }
+  }, []);
+
+  const getCardsAbandonoAcumulado = (abandonos) => {
+    const abandonosUltimoPeriodo = abandonos
+      .filter(({ periodo, estabelecimento }) => periodo === "Último período" && estabelecimento !== "Todos");
+
+    return (
+      <>
+        <GraficoInfo
+          titulo="Abandono acumulado"
+          descricao={ `Conjunto de usuários com 1° procedimento em ${abandonosUltimoPeriodo[0].a_partir_do_mes}/${abandonosUltimoPeriodo[0].a_partir_do_ano} e abandono até ${abandonosUltimoPeriodo[0].ate_mes}/${abandonosUltimoPeriodo[0].ate_ano}` }
+          fonte="Fonte: RAAS/SIASUS - Elaboração Impulso Gov"
+        />
+
+        <Grid12Col
+          items={
+            abandonosUltimoPeriodo.map((item) => (
+              <CardInfoTipoA
+                titulo={ item.estabelecimento }
+                indicador={ item.usuarios_coorte_nao_aderiram_perc }
+                indicadorSimbolo="%"
+                key={ uuidv1() }
+              />
+            ))
+          }
+          proporcao="4-4-4"
+        />
+      </>
+    );
+  };
+
   return (
     <div>
       <TituloSmallTexto
@@ -25,10 +67,9 @@ const TaxaAbandono = () => {
         titulo="<strong>Taxa de não adesão</strong>"
       />
 
-      <GraficoInfo
-        titulo="Referência de Saúde Mental"
-        fonte="Fonte: BPA/SIASUS - Elaboração Impulso Gov"
-      />
+      { abandonoCoortes.length !== 0 &&
+        getCardsAbandonoAcumulado(abandonoCoortes)
+      }
 
       <GraficoInfo
         titulo="Atendimentos"
