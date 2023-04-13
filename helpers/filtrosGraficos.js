6bc1917398933a622024d4b8d1d6313cdbbc3675
Taina Santana <tainnaps@gmail.com>
2023-04-12 23:59:45 -0300

    add helpers de filtros dos gráficos

---
+++ helpers/filtrosGraficos.js
@@ -0,0 +1,66 @@
+import { components } from "react-select";
+
+export const getPropsFiltroEstabelecimento = (dados, estadoFiltro, funcaoSetFiltro) => {
+  const options = dados
+    .map(({ estabelecimento }) => ({
+      value: estabelecimento,
+      label: estabelecimento
+    }));
+
+  const optionPersonalizada = ({ children, ...props }) => (
+    <components.Control { ...props }>
+      Estabelecimento: { children }
+    </components.Control>
+  );
+
+  return {
+    options,
+    defaultValue: estadoFiltro,
+    selectedValue: estadoFiltro,
+    onChange: (selected) => funcaoSetFiltro({
+      value: selected.value,
+      label: selected.value
+    }),
+    isMulti: false,
+    isSearchable: false,
+    components: { Control: optionPersonalizada },
+    styles: { control: (css) => ({ ...css, paddingLeft: '15px' }) },
+  };
+};
+
+export const getPropsFiltroPeriodoMulti = (dados, estadoFiltro, funcaoSetFiltro) => {
+  const periodosSemDuplicadas = [];
+
+  dados.forEach(({ periodo, competencia }) => {
+    const periodoEncontrado = periodosSemDuplicadas
+      .find((item) => item.periodo === periodo);
+
+    if (!periodoEncontrado) {
+      periodosSemDuplicadas.push({ periodo, competencia });
+    }
+  });
+
+  const periodosOrdenadosDesc = periodosSemDuplicadas
+    .sort((a, b) => new Date(b.competencia) - new Date(a.competencia))
+    .map(({ periodo }) => ({
+      value: periodo,
+      label: periodo
+    }));
+
+  const optionPersonalizada = ({ children, ...props }) => (
+    <components.Control { ...props }>
+      Períodos: { children }
+    </components.Control>
+  );
+
+  return {
+    options: periodosOrdenadosDesc,
+    defaultValue: estadoFiltro,
+    selectedValue: estadoFiltro,
+    onChange: (selected) => funcaoSetFiltro(selected),
+    isMulti: true,
+    isSearchable: false,
+    components: { Control: optionPersonalizada },
+    styles: { control: (css) => ({ ...css, paddingLeft: '15px' }) },
+  };
+};
