837c98076f23a9a5c5f611e42f95adcd439391a5
Taina Santana <tainnaps@gmail.com>
2023-04-13 14:39:55 -0300

    dados redução de danos resumo

--- pages/outros-raps/resumo/index.js
+++ pages/outros-raps/resumo/index.js
@@ -3,9 +3,11 @@ import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { v1 as uuidv1 } from 'uuid';
 import { redirectHomeNotLooged } from "../../../helpers/RedirectHome";
-import { getAtendimentosConsultorioNaRua, getAtendimentosConsultorioNaRua12meses } from "../../../requests/outros-raps";
+import { getAcoesReducaoDeDanos, getAcoesReducaoDeDanos12meses, getAtendimentosConsultorioNaRua, getAtendimentosConsultorioNaRua12meses } from "../../../requests/outros-raps";
 import naRuaJSON from "../dadosrecife/consultorioNaRua.json";
 import naRua12MesesJSON from "../dadosrecife/consultorioNaRua12.json";
+import reducaoDeDanosJSON from "../dadosrecife/reducaoDeDanos.json";
+import reducaoDeDanos12JSON from "../dadosrecife/reducaoDeDanos12.json";
 
 export function getServerSideProps(ctx) {
   const redirect = redirectHomeNotLooged(ctx);
@@ -19,16 +21,22 @@ const Resumo = () => {
   const { data: session } = useSession();
   const [consultorioNaRua, setConsultorioNaRua] = useState([]);
   const [consultorioNaRua12Meses, setConsultorioNaRua12Meses] = useState([]);
+  const [reducaoDanos, setReducaoDanos] = useState([]);
+  const [reducaoDanos12Meses, setReducaoDanos12Meses] = useState([]);
 
   useEffect(() => {
     const getDados = async (municipioIdSus) => {
       if (municipioIdSus === '261160') {
         setConsultorioNaRua(naRuaJSON);
         setConsultorioNaRua12Meses(naRua12MesesJSON);
+        setReducaoDanos(reducaoDeDanosJSON);
+        setReducaoDanos12Meses(reducaoDeDanos12JSON);
       }
       else {
         setConsultorioNaRua(await getAtendimentosConsultorioNaRua(municipioIdSus));
         setConsultorioNaRua12Meses(await getAtendimentosConsultorioNaRua12meses(municipioIdSus));
+        setReducaoDanos(await getAcoesReducaoDeDanos(municipioIdSus));
+        setReducaoDanos12Meses(await getAcoesReducaoDeDanos12meses(municipioIdSus));
       }
     };
 
@@ -45,6 +53,24 @@ const Resumo = () => {
   const getDadosConsultorioNaRua12meses = () => {
     return consultorioNaRua12Meses.find((item) =>
       item.tipo_producao === "Todos");
+  };
+
+  const getDadosReducaoDanos = () => {
+    return reducaoDanos.find((item) =>
+      item.periodo === "Último período"
+      && item.estabelecimento === "Todos"
+      && item.estabelecimento_linha_perfil === "Todas"
+      && item.estabelecimento_linha_idade === "Todas"
+    );
+  };
+
+  const getDadosReducaoDanos12meses = () => {
+    return reducaoDanos12Meses.find((item) =>
+      item.estabelecimento === "Todos"
+      && item.estabelecimento_linha_perfil === "Todas"
+      && item.profissional_vinculo_ocupacao === "Todas"
+      && item.estabelecimento_linha_idade === "Todas"
+    );
   };
 
   return (
@@ -112,7 +138,7 @@ const Resumo = () => {
             }
           </>,
           <>
-            {
+            { consultorioNaRua12Meses.length !== 0 &&
               <CardInfoTipoA
                 key={ uuidv1() }
                 indicador={ getDadosConsultorioNaRua12meses().quantidade_registrada }
@@ -134,23 +160,23 @@ const Resumo = () => {
       <Grid12Col
         items={ [
           <>
-            {
+            { reducaoDanos.length !== 0 &&
               <CardInfoTipoA
                 key={ uuidv1() }
-                indicador={ 100 }
-                titulo={ `Total de ações de redução de danos em Julho)` }
-                indice={ -141 }
+                indicador={ getDadosReducaoDanos().quantidade_registrada }
+                titulo={ `Total de ações de redução de danos em ${getDadosReducaoDanos().nome_mes}` }
+                indice={ getDadosReducaoDanos().dif_quantidade_registrada_anterior }
                 indiceDescricao="últ. mês"
               />
             }
           </>,
           <>
-            {
+            { reducaoDanos12Meses.length !== 0 &&
               <CardInfoTipoA
                 key={ uuidv1() }
-                indicador={ 100 }
-                titulo={ `Total ações de redução de danos entre Julho/2022 e Julho/2022` }
-                indice={ -141 }
+                indicador={ getDadosReducaoDanos12meses().quantidade_registrada }
+                titulo={ `Total ações de redução de danos entre ${getDadosReducaoDanos12meses().a_partir_do_mes}/${getDadosReducaoDanos12meses().a_partir_do_ano} e ${getDadosReducaoDanos12meses().ate_mes}/${getDadosReducaoDanos12meses().ate_ano}` }
+                indice={ getDadosReducaoDanos12meses().dif_quantidade_registrada_anterior }
                 indiceDescricao="doze meses anteriores"
               />
             }
