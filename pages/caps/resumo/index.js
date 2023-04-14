5a6efaa39987c8a4fde195bd01ef726971e86ea8
Taina Santana <tainnaps@gmail.com>
2023-04-14 13:15:41 -0300

    add cards atendimentos resumo caps

--- pages/caps/resumo/index.js
+++ pages/caps/resumo/index.js
@@ -15,6 +15,7 @@ import { redirectHomeNotLooged } from "../../../helpers/RedirectHome";
 import Select from "react-select";
 import { getPropsFiltroEstabelecimento } from "../../../helpers/filtrosGraficos";
 import styles from "../Caps.module.css";
+import atendPerfilResumo from "../atendimentoindividuais/perfilResumo.json";
 import atendPorCaps from "../atendimentoindividuais/porCaps.json";
 import novosResumo from "../novosusuarios/novosResumo.json";
 import perfPorEstabelecimento from "../perfildousuario/perfilPorEstabelecimento.json";
@@ -36,6 +37,7 @@ const Resumo = () => {
   const [procedimentoPorTempoServicoResumo, setProcedimentosPorTempoServicoResumo] = useState(procePorTempoServicoResumo[0]);
   const [procedimentosPorEstabelecimento, setProcedimentosPorEstabelecimento] = useState(procePorEstabelecimento);
   const [atendimentoPorCaps, setAtendimentoPorCaps] = useState(atendPorCaps);
+  const [atendimentoPerfilResumo, setAtendimentoPerfilResumo] = useState(atendPerfilResumo[0]);
   const [novosUsuariosResumo, setNovosUsuariosResumo] = useState(novosResumo);
   const [perfilPorEstabelecimento, setPerfilPorEstabelecimento] = useState(perfPorEstabelecimento);
   const [filtroEstabelecimento, setFiltroEstabelecimento] = useState({
@@ -133,6 +135,68 @@ const Resumo = () => {
     );
   };
 
+  const getCardsAtendimentosIndividuais = (atendimentosPorEstabelecimento, atendimentosPerfil) => {
+    const atendimentosUltimoPeriodo = getDadosUltimoPeriodo(
+      atendimentosPorEstabelecimento
+    );
+    const atendimentosTodosEstabelecimentos = getDadosDeEstabelecimentoFiltrado(
+      atendimentosUltimoPeriodo,
+      "Todos"
+    );
+
+    const {
+      perc_apenas_atendimentos_individuais: percAtendimentosIndividuais,
+      dif_perc_apenas_atendimentos_individuais: difPercAtendimentosIndividuais,
+      maior_taxa_estabelecimento: estabelecimentoMaiorTaxa,
+      maior_taxa: maiorTaxa
+    } = atendimentosTodosEstabelecimentos;
+
+
+    const {
+      sexo_predominante: sexoPredominante,
+      faixa_etaria_predominante: faixaEtariaPredominante,
+      cid_grupo_predominante: cidPredominante,
+      usuarios_cid_predominante: quantidadeUsuariosComCid
+    } = atendimentosPerfil;
+    return (
+      <Grid12Col
+        proporcao="3-3-3-3"
+        items={ [
+          <>
+            <CardInfoTipoA
+              indicador={ percAtendimentosIndividuais }
+              indice={ difPercAtendimentosIndividuais }
+              indiceDescricao="p.p. semestre anterior"
+              indicadorSimbolo="%"
+            />
+          </>,
+          <>
+            <CardInfoTipoA
+              indicador={ maiorTaxa }
+              fonte={ estabelecimentoMaiorTaxa }
+              indicadorSimbolo="%"
+              titulo="CAPS com maior taxa"
+            />
+          </>,
+          <>
+            <CardInfoTipoA
+              indicador={ faixaEtariaPredominante }
+              fonte={ sexoPredominante }
+              titulo="Perfil do usuário"
+            />
+          </>,
+          <>
+            <CardInfoTipoA
+              indicador={ quantidadeUsuariosComCid }
+              fonte={ cidPredominante }
+              titulo="CID mais frequente"
+            />
+          </>,
+        ] }
+      />
+    );
+  };
+
   return (
     <div>
       <TituloSmallTexto
@@ -220,47 +284,19 @@ const Resumo = () => {
       <GraficoInfo
         titulo="Usuários que realizaram apenas atendimentos individuais"
         fonte="Fonte: BPA-i e RAAS/SIASUS - Elaboração Impulso Gov"
-        descricao="Dados de Setembro"
+        descricao={ `Dados de ${atendimentoPerfilResumo.nome_mes}` }
         tooltip="Porcentagem do total de usuários que frequentaram serviços CAPS no mês que realizou apenas atendimentos individuais."
         link={ { label: 'Mais informações', url: '/caps?painel=4' } }
       />
 
-      <Grid12Col
-        proporcao="3-3-3-3"
-        items={ [
-          <>
-            <CardInfoTipoA
-              indicador='2'
-              indice='10'
-              indiceDescricao="p.p. semestre anterior"
-              indicadorSimbolo="%"
-            />
-          </>,
-          <>
-            <CardInfoTipoA
-              indicador='85'
-              fonte='Vida'
-              indicadorSimbolo="%"
-              titulo="CAPS com maior taxa"
-            />
-          </>,
-          <>
-            <CardInfoTipoA
-              indicador='30 a 40 anos'
-              fonte='Masculino'
-              titulo="Perfil do usuário"
-            />
-          </>,
-          <>
-            <CardInfoTipoA
-              indicador='22 usuários'
-              fonte='Uso de subs. psicoativa'
-              titulo="CID mais frequente"
-            />
-          </>,
-        ] }
-      />
-
+      {
+        atendimentoPorCaps.length !== 0 &&
+        atendimentoPerfilResumo &&
+        getCardsAtendimentosIndividuais(
+          atendimentoPorCaps,
+          atendimentoPerfilResumo
+        )
+      }
 
       <GraficoInfo
         titulo="Procedimento por usuário"
