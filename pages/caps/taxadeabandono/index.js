7d87652937481610f0a65b7dcd1958b9d77e5895
Taina Santana <tainnaps@gmail.com>
2023-05-02 14:42:53 -0300

    ajusta titulo do histórico temporal não adesão

--- pages/caps/taxadeabandono/index.js
+++ pages/caps/taxadeabandono/index.js
@@ -150,7 +150,7 @@ const TaxaAbandono = () => {
       }
 
       <GraficoInfo
-        titulo="Histórico Temporal"
+        titulo="Histórico Temporal - Taxa mensal"
         descricao="Dos usuários acolhidos há menos de 3 meses, quantos não aderiram ao serviço no mês"
         tooltip="Diferente do indicador de não adesão acumulado, que mostra o percentual de usuários que iniciaram o vínculo em um determinado mês e em até 3 meses deixaram de frequentar o CAPS, a taxa de não adesão mensal mostra o percentual de usuários recentes que deixaram de frequentar o serviço em um mês específico. Ou seja, o indicador mensal mostra qual foi o mês que o usuário iniciou o período de inatividade (que precisa ser igual ou superior a 3 meses)."
         fonte="Fonte: RAAS/SIASUS - Elaboração Impulso Gov"
