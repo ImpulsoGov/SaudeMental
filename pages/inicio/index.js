e9b23af1913501b95114bf2bcdc3bc0591bc26ee
Taina Santana <tainnaps@gmail.com>
2023-05-10 14:18:45 -0300

    Revert "add spinner após login"
    This reverts commit 4dc5655f380890597d7bd924280b3b7b3b0d3b87.

--- pages/inicio/index.js
+++ pages/inicio/index.js
@@ -1,35 +1,38 @@
-import { CardLargeGrid, CardLargeGridInicioSM, Greeting, Spinner } from "@impulsogov/design-system";
+import { CardLargeGrid, Greeting, CardLargeGridInicioSM } from "@impulsogov/design-system";
 import { useSession } from "next-auth/react";
 import { redirectHomeNotLooged } from "../../helpers/RedirectHome";
 import { HOME } from "../../querys/HOME";
 import { getData } from "../../services/getData";
 
 export async function getServerSideProps(ctx) {
-  const redirect = redirectHomeNotLooged(ctx);
-  if (redirect) return redirect;
+  const redirect = redirectHomeNotLooged(ctx)
+  if(redirect) return redirect
 
   const res = [await getData(HOME)];
 
   return {
-    props: {
-      res
-    }
-  };
+      props: {
+        res
+      }
+  }
 }
 
-function Inicio({ res, session }) {
+
+function Inicio({res}) {
+  const { data: session } = useSession();
+
   return (
     <>
       <Greeting
-        cargo={ session?.user.cargo }
+        cargo={session?.user.cargo}
         greeting="Boas vindas"
-        municipio_uf={ session?.user.municipio }
-        nome_usuario={ session?.user.nome }
+        municipio_uf={session?.user.municipio}
+        nome_usuario={session?.user.nome}
         texto="Aqui você pode acompanhar a situação das RAPS e demais serviços de saúde mental do seu município, monitorando a qualidade do cuidado prestado e aprofundando seu diagnóstico sobre o território e a população atendida."
       />
 
       <CardLargeGrid
-        cards={ [
+        cards={[
           {
             icon: 'https://media.graphassets.com/zCC2wPnJTxagLyEZFV45',
             links: res[0].cards[0].body.map((item) => ({
@@ -54,12 +57,12 @@ function Inicio({ res, session }) {
             })),
             titulo: res[0].cards[2].title
           }
-        ] }
+        ]}
         obs=""
         theme="ColorSM"
       />
       <CardLargeGridInicioSM
-        cards={ [
+        cards={[
           {
             icon: 'https://media.graphassets.com/mKRm7fObQ5xJ44Zto9cU',
             link: '/materiais-complementares',
@@ -72,8 +75,8 @@ function Inicio({ res, session }) {
             texto: 'Aqui você pode ver respostas para as dúvidas mais frequentes e solicitar ajuda para o nosso time.',
             titulo: 'Central de ajuda'
           }
-        ] }
-        cardsExtra={ [
+        ]}
+        cardsExtra={[
           {
             icon: 'https://media.graphassets.com/7xPUwf12RjSiIrteLkwa',
             link: '/gestao-usuarios',
@@ -84,28 +87,13 @@ function Inicio({ res, session }) {
             link: '/duvidas',
             titulo: 'Feedbacks e sugestões'
           }
-        ] }
+        ]}
         obs="Para sair da área logada, basta ir no seu usuário no menu superior e clicar em ‘SAIR’."
         theme="ColorSM"
       />
     </>
-  );
+  )
 }
 
-function Index({ res }) {
-  const { data: session } = useSession();
 
-  return (
-    <>
-      {
-        session
-          ? <Inicio res={ res } session={ session } />
-          : <div style={ { height: "70vh", display: "flex", alignItems: "center" } }>
-            <Spinner theme="SM" />
-          </div>
-      }
-    </>
-  );
-}
-
-export default Index;
+export default Inicio;
