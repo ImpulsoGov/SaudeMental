b527e135d15dcd7a7f153e6a5c7eaa13e9ecc90a
Taina Santana <tainnaps@gmail.com>
2023-05-22 20:07:42 -0300

    testa visibilidade de input de email

--- cypress/e2e/PrimeiroAcesso.cy.js
+++ cypress/e2e/PrimeiroAcesso.cy.js
@@ -4,17 +4,26 @@ describe('Primeiro acesso', () => {
   context('resolução 1440 x 900', () => {
     beforeEach(() => {
       cy.viewport(1440, 900);
+      cy.visit('https://saudemental.impulsogov.org/');
     });
 
     it('O botão de primeiro acesso é exibido ao clicar em "Entrar"', () => {
-      cy.visit('https://saudemental.impulsogov.org/');
-
       cy.contains('Entrar').click();
 
       cy.get('.NavBar_NavBarModalContainer__tePj9')
         .find('.ButtonLight_ButtonLightContainer__w0rNI')
         .should('be.visible')
         .and('have.text', 'PRIMEIRO ACESSO');
+    });
+
+    it('É exibida a input de email ao clicar no botão de Primeiro Acesso', () => {
+      cy.contains('Entrar').click();
+
+      cy.get('.NavBar_NavBarModalContainer__tePj9')
+        .find('.ButtonLight_ButtonLightContainer__w0rNI')
+        .click();
+
+      cy.get('input[placeholder*="E-mail"]').should('be.visible');
     });
   });
 });
