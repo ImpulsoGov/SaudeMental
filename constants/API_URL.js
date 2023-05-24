11ca591263245ef4c556d4ea047359f33d5a3b6e
Taina Santana <tainnaps@gmail.com>
2023-05-24 15:21:10 -0300

    usa api local

--- constants/API_URL.js
+++ constants/API_URL.js
@@ -1,2 +1,3 @@
-export const API_URL = 'https://api-territorios.herokuapp.com/';
+// export const API_URL = 'https://api-territorios.herokuapp.com/';
+export const API_URL = 'http://127.0.0.1:8000/';
 
