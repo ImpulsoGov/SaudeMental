86300cbe001348914cb29398d5a5e6fd0c88ed25
Taina Santana <tainnaps@gmail.com>
2023-05-26 01:19:33 -0300

    fix url da api

--- constants/API_URL.js
+++ constants/API_URL.js
@@ -1,3 +1,2 @@
-// export const API_URL = 'https://api-territorios.herokuapp.com/';
-export const API_URL = 'http://127.0.0.1:8000/';
+export const API_URL = 'https://api-territorios.herokuapp.com/';
 
