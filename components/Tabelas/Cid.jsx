c6c6a82ff2442a08a2441f38e6b551f439856a64
Taina Santana <tainnaps@gmail.com>
2023-07-27 11:26:02 -0300

    feat: cria tabela cid

---
+++ components/Tabelas/Cid.jsx
@@ -0,0 +1,88 @@
+import { DataGrid } from '@mui/x-data-grid';
+import PropTypes from 'prop-types';
+import React, { useMemo } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import { CORES_GRAFICO_DONUT } from '../../constants/CORES_GRAFICO_DONUT';
+
+const TabelaCid = ({ labels, cids }) => {
+  const colunas = useMemo(() => [
+    {
+      field: 'condicaoSaude',
+      headerName: labels.colunaCid,
+      sortable: false,
+      flex: 190,
+      align: 'left',
+      headerAlign: 'left',
+    },
+    {
+      field: 'quantidade',
+      headerName: labels.colunaQuantidade,
+      sortable: false,
+      flex: 190,
+      align: 'right',
+      headerAlign: 'right',
+      renderCell: (params) => {
+        const { posicao, valor } = params.value;
+        return (
+          <div style={ {
+            backgroundColor: CORES_GRAFICO_DONUT[posicao],
+            width: '100%',
+            height: '100%',
+            textAlign: 'right',
+            paddingRight: '12px',
+          } }>
+            <p>{ valor }</p>
+          </div>
+        );
+      }
+    },
+  ], [labels]);
+
+  const linhas = useMemo(() => cids.map(({ condicaoSaude, quantidade }, index) => ({
+    id: uuidV4(),
+    condicaoSaude,
+    quantidade: { posicao: index, valor: quantidade }
+  })), [cids]);
+
+  return (
+    <DataGrid
+      sx={ {
+        '& .MuiDataGrid-columnHeaderTitle': {
+          fontWeight: 'bold',
+          fontSize: '16px',
+          lineHeight: '1.2rem',
+          whiteSpace: 'normal',
+          textAlign: 'center'
+        },
+        '& .MuiDataGrid-cell--textRight': {
+          paddingRight: 0,
+          paddingLeft: 0
+        },
+        height: '70vh',
+        border: 'none'
+      } }
+      rows={ linhas }
+      columns={ colunas }
+      disableColumnMenu
+      autoPageSize
+      initialState={ {
+        sorting: {
+          sortModel: [{ field: 'quantidade', sort: 'desc' }],
+        },
+      } }
+    />
+  );
+};
+
+TabelaCid.propTypes = {
+  labels: PropTypes.shape({
+    colunaCid: PropTypes.string,
+    colunaQuantidade: PropTypes.string,
+  }),
+  cids: PropTypes.shape({
+    condicaoSaude: PropTypes.string,
+    quantidade: PropTypes.number,
+  }),
+}.isRequired;
+
+export default TabelaCid;
