bd7f666a677dd47534fa2bc66d2ba72f469895c4
Taina Santana <tainnaps@gmail.com>
2023-07-28 13:55:30 -0300

    style: padroniza aspas simples cores gráfico donut

--- constants/GRAFICO_DONUT.js
+++ constants/GRAFICO_DONUT.js
@@ -1,4 +1,4 @@
-export const CORES_GRAFICO_DONUT = ["#5367C9", "#6577CF", "#7685D4", "#8795DA", "#98A4DF", "#A9B3E4", "#BAC2E9", "#CAD0EE", "#D3D8F1", "#E0E4F5"];
+export const CORES_GRAFICO_DONUT = ['#5367C9', '#6577CF', '#7685D4', '#8795DA', '#98A4DF', '#A9B3E4', '#BAC2E9', '#CAD0EE', '#D3D8F1', '#E0E4F5'];
 
 export const COR_GRAFICO_DONUT_SEM_DADOS = '#d3d3d3';
 
