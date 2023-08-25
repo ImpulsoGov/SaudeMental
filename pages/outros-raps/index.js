1e0ef46dd8daf49f130187246945bae8db811cfa
Taina Santana <tainnaps@gmail.com>
2023-08-25 17:42:02 -0300

    Descomenta painéis de outros raps

--- pages/outros-raps/index.js
+++ pages/outros-raps/index.js
@@ -1,14 +1,13 @@
-import { ButtonLight, TituloTexto } from "@impulsogov/design-system";
+import { ButtonLight, PanelSelectorSM, TituloTexto } from '@impulsogov/design-system';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from "react";
-// import { v1 as uuidv1 } from 'uuid';
-import { redirectHomeNotLooged } from "../../helpers/RedirectHome";
-import style from "../duvidas/Duvidas.module.css";
-// import style from "../duvidas/Duvidas.module.css";
-// import Ambulatorio from "./ambulatorio";
-// import ConsultorioNaRua from "./consultorio-na-rua";
-// import ReducaoDeDanos from "./reducao-de-danos";
-// import Resumo from "./resumo";
+import { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { redirectHomeNotLooged } from '../../helpers/RedirectHome';
+import style from '../duvidas/Duvidas.module.css';
+import Ambulatorio from './ambulatorio';
+import ConsultorioNaRua from './consultorio-na-rua';
+import ReducaoDeDanos from './reducao-de-danos';
+import Resumo from './resumo';
 
 export async function getServerSideProps(ctx) {
   const redirect = redirectHomeNotLooged(ctx);
@@ -29,7 +28,7 @@ export default function Paineis() {
       pathname: router.pathname,
       query: { painel: activeTabIndex }
     },
-      undefined, { shallow: true }
+    undefined, { shallow: true }
     );
   }, [activeTabIndex]);
 
@@ -38,13 +37,13 @@ export default function Paineis() {
       <div className={ style.BotaoVoltar }>
         <ButtonLight
           icone={ { posicao: 'right', url: 'https://media.graphassets.com/8NbkQQkyRSiouNfFpLOG' } }
-          label="VOLTAR"
-          link="/inicio" />
-        <div style={ { position: "relative", left: "70%" } }>
+          label='VOLTAR'
+          link='/inicio' />
+        <div style={ { position: 'relative', left: '70%' } }>
           <ButtonLight
             icone={ { posicao: 'right', url: 'https://media.graphassets.com/yaYuyi0KS3WkqhKPUzro' } }
-            label="CENTRAL DE AJUDA"
-            link="/central-de-ajuda" />
+            label='CENTRAL DE AJUDA'
+            link='/central-de-ajuda' />
         </div>
       </div>
 
@@ -53,17 +52,8 @@ export default function Paineis() {
           posicao: null,
           url: ''
         } }
-        texto="Olá! Nessa parte do painel você encontrará informações relativas ao Ambulatório de Saúde Mental, Consultório na Rua e ações de Redução de Danos do seu município. Lançaremos esses dados nas próximas semanas e, assim que eles estiverem disponíveis, avisaremos você por e-mail e whatsapp."
-        titulo="<strong>EM BREVE</strong>"
-      />
-
-      {/* <TituloTexto
-        imagem={ {
-          posicao: null,
-          url: ''
-        } }
-        titulo="Outros serviços RAPS"
-        texto=""
+        titulo='Outros serviços RAPS'
+        texto=''
       />
 
       <PanelSelectorSM
@@ -75,24 +65,24 @@ export default function Paineis() {
           setActiveTitleTabIndex: setActiveTitleTabIndex
         } }
         components={ [[
-          <Resumo key={ uuidv1() } />,
-          <Ambulatorio key={ uuidv1() } />,
-          <ConsultorioNaRua key={ uuidv1() } />,
-          <ReducaoDeDanos key={ uuidv1() } />
+          <Resumo key={ uuidv4() } />,
+          <Ambulatorio key={ uuidv4() } />,
+          <ConsultorioNaRua key={ uuidv4() } />,
+          <ReducaoDeDanos key={ uuidv4() } />
         ]] }
         subtitles={ [
           [
             {
-              label: "RESUMO",
+              label: 'RESUMO',
             },
             {
-              label: "AMBULATÓRIO DE SAÚDE MENTAL",
+              label: 'AMBULATÓRIO DE SAÚDE MENTAL',
             },
             {
-              label: "CONSULTÓRIO NA RUA",
+              label: 'CONSULTÓRIO NA RUA',
             },
             {
-              label: "REDUÇÃO DE DANOS",
+              label: 'REDUÇÃO DE DANOS',
             },
           ]
         ] }
@@ -101,7 +91,7 @@ export default function Paineis() {
             label: ''
           }
         ] }
-      /> */}
+      />
     </div>
   );
 }
