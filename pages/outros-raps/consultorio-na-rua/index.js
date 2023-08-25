0dc1c83ce9d4f0c4574926bce48eb933cfff0547
Taina Santana <tainnaps@gmail.com>
2023-08-25 17:43:02 -0300

    Remove agregações e usa componentes de filtro

--- pages/outros-raps/consultorio-na-rua/index.js
+++ pages/outros-raps/consultorio-na-rua/index.js
@@ -1,12 +1,14 @@
-import { CardInfoTipoA, GraficoInfo, Grid12Col, TituloSmallTexto } from "@impulsogov/design-system";
-import ReactEcharts from "echarts-for-react";
-import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
-import Select, { components } from "react-select";
+import { CardInfoTipoA, GraficoInfo, Grid12Col, TituloSmallTexto } from '@impulsogov/design-system';
+import ReactEcharts from 'echarts-for-react';
+import { useSession } from 'next-auth/react';
+import { useEffect, useMemo, useState } from 'react';
 import { v1 as uuidv1 } from 'uuid';
-import { redirectHomeNotLooged } from "../../../helpers/RedirectHome";
-import { getAtendimentosConsultorioNaRua, getAtendimentosConsultorioNaRua12meses } from "../../../requests/outros-raps";
-import styles from "../OutrosRaps.module.css";
+import { redirectHomeNotLooged } from '../../../helpers/RedirectHome';
+import { getAtendimentosConsultorioNaRua, getAtendimentosConsultorioNaRua12meses } from '../../../requests/outros-raps';
+import styles from '../OutrosRaps.module.css';
+import { FiltroTexto, FiltroCompetencia } from '../../../components/Filtros';
+import { getOpcoesGraficoDonut } from '../../../helpers/graficoDonut';
+import { TabelaGraficoDonut } from '../../../components/Tabelas';
 
 export function getServerSideProps(ctx) {
   const redirect = redirectHomeNotLooged(ctx);
@@ -21,10 +23,10 @@ const ConsultorioNaRua = () => {
   const [atendimentos, setAtendimentos] = useState([]);
   const [atendimentos12meses, setAtendimentos12meses] = useState([]);
   const [filtroProducao, setFiltroProducao] = useState({
-    value: "Todos", label: "Todos"
+    value: 'Todos', label: 'Todos'
   });
   const [filtroCompetencia, setFiltroCompetencia] = useState({
-    value: "Último período", label: "Último período"
+    value: 'Último período', label: 'Último período'
   });
 
   useEffect(() => {
@@ -40,37 +42,13 @@ const ConsultorioNaRua = () => {
     }
   }, []);
 
-  const agregarPorProducao = (atendimentos) => {
-    const atendimentosAgregados = [];
-
-    atendimentos.forEach((atendimento) => {
-      const { tipo_producao: tipoProducao, competencia, periodo, quantidade_registrada: quantidadeRegistrada } = atendimento;
-      const atendimentoEncontrado = atendimentosAgregados.find((item) => item.tipoProducao === tipoProducao);
-
-      if (!atendimentoEncontrado) {
-        atendimentosAgregados.push({
-          tipoProducao,
-          quantidadesPorPeriodo: [{ competencia, periodo, quantidadeRegistrada }]
-        });
-      } else {
-        atendimentoEncontrado.quantidadesPorPeriodo.push({ competencia, periodo, quantidadeRegistrada });
-      }
-    });
-
-    return atendimentosAgregados;
-  };
-
   const ordenarQuantidadesPorCompetenciaAsc = (atendimentos) => {
-    return atendimentos.map(({ tipoProducao, quantidadesPorPeriodo }) => ({
-      tipoProducao,
-      quantidadesPorPeriodo: quantidadesPorPeriodo
-        .sort((a, b) => new Date(a.competencia) - new Date(b.competencia))
-    }));
+    return atendimentos.sort((a, b) => new Date(a.competencia) - new Date(b.competencia));
   };
 
   const getOpcoesGraficoDeLinha = (atendimentos) => {
-    const atendimentosAgregados = agregarPorProducao(atendimentos);
-    const atendimentosOrdenados = ordenarQuantidadesPorCompetenciaAsc(atendimentosAgregados);
+    const atendimentosFiltrados = atendimentos.filter(({ tipo_producao: tipoProducao }) => tipoProducao === filtroProducao.value);
+    const atendimentosOrdenados = ordenarQuantidadesPorCompetenciaAsc(atendimentosFiltrados);
 
     return {
       tooltip: {
@@ -84,7 +62,7 @@ const ConsultorioNaRua = () => {
       toolbox: {
         feature: {
           saveAsImage: {
-            title: "Salvar como imagem",
+            title: 'Salvar como imagem',
           }
         }
       },
@@ -96,189 +74,69 @@ const ConsultorioNaRua = () => {
       },
       xAxis: {
         type: 'category',
-        data: atendimentosOrdenados[0].quantidadesPorPeriodo.map(({ periodo }) => periodo)
+        data: atendimentosOrdenados.map(({ periodo }) => periodo)
       },
       yAxis: {
         type: 'value'
       },
       series: [
         {
-          name: "Atendimentos realizados",
+          name: 'Atendimentos realizados',
           data: atendimentosOrdenados
-            .find(({ tipoProducao }) => tipoProducao === filtroProducao.value).quantidadesPorPeriodo
-            .map(({ quantidadeRegistrada }) => quantidadeRegistrada),
+            .map(({ quantidade_registrada: quantidadeRegistrada }) => quantidadeRegistrada),
           type: 'line',
           itemStyle: {
-            color: "#5367C9"
+            color: '#5367C9'
           },
         }
       ]
     };
   };
 
-  const agregarPorPeriodo = (atendimentos) => {
-    const atendimentosAgregados = [];
+  const atendimentosFiltradosOrdenados = useMemo(() => {
+    const atendimentosFiltrados = atendimentos.filter(({
+      tipo_producao: tipoProducao,
+      periodo
+    }) =>
+      tipoProducao !== 'Todos' && periodo === filtroCompetencia.value
+    );
 
-    atendimentos.forEach((atendimento) => {
-      const { tipo_producao: tipoProducao, competencia, periodo, quantidade_registrada: quantidadeRegistrada } = atendimento;
-      const atendimentoEncontrado = atendimentosAgregados.find((item) => item.periodo === periodo);
+    const atendimentosOrdenados = atendimentosFiltrados
+      .sort((a, b) => b.quantidade_registrada - a.quantidade_registrada);
 
-      if (!atendimentoEncontrado) {
-        atendimentosAgregados.push({
-          periodo,
-          competencia,
-          quantidadesPorProducao: [{ tipoProducao, quantidadeRegistrada }]
-        });
-      } else {
-        atendimentoEncontrado.quantidadesPorProducao.push({ tipoProducao, quantidadeRegistrada });
-      }
-    });
+    const atendimentosFormatados = atendimentosOrdenados.map(({
+      tipo_producao: nome,
+      quantidade_registrada: quantidade
+    }) => ({
+      nome, quantidade
+    }));
 
-    return atendimentosAgregados;
-  };
-
-  const getOpcoesGraficoDonut = (atendimentos) => {
-    const atendimentosAgregados = agregarPorPeriodo(atendimentos);
-
-    const { quantidadesPorProducao } = atendimentosAgregados
-      .find(({ periodo }) => periodo === filtroCompetencia.value);
-
-    const quantidadesPorProducaoFiltradas = quantidadesPorProducao
-      .filter(({ tipoProducao }) => tipoProducao !== "Todos")
-      .sort((a, b) => a.tipoProducao.localeCompare(b.tipoProducao));
-
-    return {
-      tooltip: {
-        trigger: 'item',
-      },
-      series: [
-        {
-          type: 'pie',
-          radius: ['40%', '80%'],
-          avoidLabelOverlap: false,
-          label: {
-            show: true,
-            position: 'inside',
-            formatter: "{d}%",
-            color: "#000000"
-          },
-          emphasis: {
-            label: {
-              show: true,
-            }
-          },
-          labelLine: {
-            show: false
-          },
-          data: [
-            {
-              value: quantidadesPorProducaoFiltradas[0].quantidadeRegistrada,
-              name: quantidadesPorProducaoFiltradas[0].tipoProducao,
-              itemStyle: {
-                color: "#5367C9"
-              },
-            },
-            {
-              value: quantidadesPorProducaoFiltradas[1].quantidadeRegistrada,
-              name: quantidadesPorProducaoFiltradas[1].tipoProducao,
-              itemStyle: {
-                color: "#6577CF"
-              },
-            },
-            {
-              value: quantidadesPorProducaoFiltradas[2].quantidadeRegistrada,
-              name: quantidadesPorProducaoFiltradas[2].tipoProducao,
-              itemStyle: {
-                color: "#7685D4"
-              },
-            },
-            {
-              value: quantidadesPorProducaoFiltradas[3].quantidadeRegistrada,
-              name: quantidadesPorProducaoFiltradas[3].tipoProducao,
-              itemStyle: {
-                color: "#8795DA"
-              },
-            },
-          ]
-        }
-      ]
-    };
-  };
+    return atendimentosFormatados;
+  }, [atendimentos, filtroCompetencia.value]);
 
   const getPropsCardUltimoPeriodo = () => {
     const atendimentoTodosUltimoPeriodo = atendimentos
-      .find((atendimento) => atendimento.tipo_producao === "Todos" && atendimento.periodo === "Último período");
+      .find((atendimento) => atendimento.tipo_producao === 'Todos' && atendimento.periodo === 'Último período');
 
     return {
       key: uuidv1(),
-      indicador: atendimentoTodosUltimoPeriodo["quantidade_registrada"],
-      titulo: `Total de atendimentos em ${atendimentoTodosUltimoPeriodo["nome_mes"]}`,
-      indice: atendimentoTodosUltimoPeriodo["dif_quantidade_registrada_anterior"],
-      indiceDescricao: "últ. mês"
+      indicador: atendimentoTodosUltimoPeriodo['quantidade_registrada'],
+      titulo: `Total de atendimentos em ${atendimentoTodosUltimoPeriodo['nome_mes']}`,
+      indice: atendimentoTodosUltimoPeriodo['dif_quantidade_registrada_anterior'],
+      indiceDescricao: 'últ. mês'
     };
   };
 
   const getPropsCardUltimos12Meses = () => {
     const atendimentoTodosUltimos12Meses = atendimentos12meses
-      .find(({ tipo_producao: tipoProducao }) => tipoProducao === "Todos");
+      .find(({ tipo_producao: tipoProducao }) => tipoProducao === 'Todos');
 
     return {
       key: uuidv1(),
-      indicador: atendimentoTodosUltimos12Meses["quantidade_registrada"],
-      titulo: "Total de atendimentos nos últimos 12 meses",
-      indice: atendimentoTodosUltimos12Meses["dif_quantidade_registrada_anterior"],
-      indiceDescricao: "doze meses anteriores"
-    };
-  };
-
-  const getPropsFiltroCompetencia = (atendimentos) => {
-    const periodosOrdemDesc = agregarPorPeriodo(atendimentos)
-      .sort((a, b) => new Date(b.competencia) - new Date(a.competencia))
-      .map(({ periodo }) => ({ value: periodo, label: periodo }));
-
-    const optionPersonalizada = ({ children, ...props }) => (
-      <components.Control { ...props }>
-        Competência: { children }
-      </components.Control>
-    );
-
-    return {
-      options: periodosOrdemDesc,
-      defaultValue: filtroCompetencia,
-      selectedValue: filtroCompetencia,
-      onChange: (selected) => setFiltroCompetencia({
-        value: selected.value,
-        label: selected.value
-      }),
-      isMulti: false,
-      isSearchable: false,
-      components: { Control: optionPersonalizada },
-      styles: { control: (css) => ({ ...css, paddingLeft: '15px' }) },
-    };
-  };
-
-  const getPropsFiltroProducao = (atendimentos) => {
-    const atendimentosAgregados = agregarPorProducao(atendimentos)
-      .map(({ tipoProducao }) => ({ value: tipoProducao, label: tipoProducao }));
-
-    const optionPersonalizada = ({ children, ...props }) => (
-      <components.Control { ...props }>
-        Tipo de produção: { children }
-      </components.Control>
-    );
-
-    return {
-      options: atendimentosAgregados,
-      defaultValue: filtroProducao,
-      selectedValue: filtroProducao,
-      onChange: (selected) => setFiltroProducao({
-        value: selected.value,
-        label: selected.value
-      }),
-      isMulti: false,
-      isSearchable: false,
-      components: { Control: optionPersonalizada },
-      styles: { control: (css) => ({ ...css, paddingLeft: '15px' }) },
+      indicador: atendimentoTodosUltimos12Meses['quantidade_registrada'],
+      titulo: 'Total de atendimentos nos últimos 12 meses',
+      indice: atendimentoTodosUltimos12Meses['dif_quantidade_registrada_anterior'],
+      indiceDescricao: 'doze meses anteriores'
     };
   };
 
@@ -289,19 +147,19 @@ const ConsultorioNaRua = () => {
           posicao: null,
           url: ''
         } }
-        texto=""
+        texto=''
         botao={{
           label: '',
           url: ''
         }}
-        titulo="<strong>Consultório na Rua</strong>"
+        titulo='<strong>Consultório na Rua</strong>'
       />
 
       <GraficoInfo
-        titulo="Atendimentos realizados por equipes"
-        fonte="Fonte: SISAB - Elaboração Impulso Gov"
-        descricao="Equipes de Consultório na Rua habilitadas no Ministério da Saúde​"
-        tooltip="Total de contatos assistenciais promovidos por todas as equipes de CnR ao longo de um período, considerados atendimentos individuais, odontológicos, procedimentos e visitas domiciliares."
+        titulo='Atendimentos realizados por equipes'
+        fonte='Fonte: SISAB - Elaboração Impulso Gov'
+        descricao='Equipes de Consultório na Rua habilitadas no Ministério da Saúde​'
+        tooltip='Total de contatos assistenciais promovidos por todas as equipes de CnR ao longo de um período, considerados atendimentos individuais, odontológicos, procedimentos e visitas domiciliares.'
       />
 
       <Grid12Col
@@ -320,39 +178,54 @@ const ConsultorioNaRua = () => {
       />
 
       <GraficoInfo
-        titulo="Tipo de produção"
-        fonte="Fonte: SISAB - Elaboração Impulso Gov"
+        titulo='Tipo de produção'
+        fonte='Fonte: SISAB - Elaboração Impulso Gov'
       />
 
       { atendimentos.length !== 0 &&
         <>
-          <div className={ styles.Filtro }>
-            <Select { ...getPropsFiltroCompetencia(atendimentos) } />
-          </div>
+          <FiltroCompetencia
+            dados={ atendimentos }
+            valor={ filtroCompetencia }
+            setValor={ setFiltroCompetencia }
+          />
 
           <div className={ styles.GraficoDonutContainer }>
             <ReactEcharts
-              option={ getOpcoesGraficoDonut(atendimentos) }
-              style={ { width: "40%", height: "100%" } }
+              option={ getOpcoesGraficoDonut(atendimentosFiltradosOrdenados) }
+              style={ { width: '50%', height: '100%' } }
+            />
+
+            <TabelaGraficoDonut
+              labels={{
+                colunaHeader: 'Tipo de produção',
+                colunaQuantidade: 'Quantidade registrada',
+              }}
+              data={ atendimentosFiltradosOrdenados }
+              mensagemDadosZerados=''
             />
           </div>
         </>
       }
 
       <GraficoInfo
-        titulo="Histórico Temporal"
-        fonte="Fonte: SISAB - Elaboração Impulso Gov"
+        titulo='Histórico Temporal'
+        fonte='Fonte: SISAB - Elaboração Impulso Gov'
       />
 
       { atendimentos.length !== 0 &&
         <>
-          <div className={ styles.Filtro }>
-            <Select { ...getPropsFiltroProducao(atendimentos) } />
-          </div>
+          <FiltroTexto
+            dados={ atendimentos }
+            label='Tipo de produção'
+            propriedade='tipo_producao'
+            valor={ filtroProducao }
+            setValor={ setFiltroProducao }
+          />
 
           <ReactEcharts
             option={ getOpcoesGraficoDeLinha(atendimentos) }
-            style={ { width: "100%", height: "70vh" } }
+            style={ { width: '100%', height: '70vh' } }
           />
         </>
       }
--- pages/outros-raps/reducao-de-danos/index.js
+++ pages/outros-raps/reducao-de-danos/index.js
@@ -1,12 +1,13 @@
-import { CardInfoTipoA, GraficoInfo, Grid12Col, TituloSmallTexto } from "@impulsogov/design-system";
-import ReactEcharts from "echarts-for-react";
-import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
-import Select, { components } from "react-select";
+import { CardInfoTipoA, GraficoInfo, Grid12Col, TituloSmallTexto } from '@impulsogov/design-system';
+import ReactEcharts from 'echarts-for-react';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 import { v1 as uuidv1 } from 'uuid';
-import { redirectHomeNotLooged } from "../../../helpers/RedirectHome";
-import { getAcoesReducaoDeDanos, getAcoesReducaoDeDanos12meses } from "../../../requests/outros-raps";
-import styles from "../OutrosRaps.module.css";
+import { redirectHomeNotLooged } from '../../../helpers/RedirectHome';
+import { getAcoesReducaoDeDanos, getAcoesReducaoDeDanos12meses } from '../../../requests/outros-raps';
+import styles from '../OutrosRaps.module.css';
+import FiltroTexto from '../../../components/Filtros/FiltroTexto';
+import { FILTRO_ESTABELECIMENTO_DEFAULT, FILTRO_OCUPACAO_DEFAULT } from '../../../constants/FILTROS';
 
 export function getServerSideProps(ctx) {
   const redirect = redirectHomeNotLooged(ctx);
@@ -20,12 +21,8 @@ const ReducaoDeDanos = () => {
   const { data: session } = useSession();
   const [acoes, setAcoes] = useState([]);
   const [acoes12meses, setAcoes12meses] = useState([]);
-  const [filtroEstabelecimento, setFiltroEstabelecimento] = useState({
-    value: "Todos", label: "Todos"
-  });
-  const [filtroOcupacao, setFiltroOcupacao] = useState({
-    value: "Todas", label: "Todas"
-  });
+  const [filtroEstabelecimento, setFiltroEstabelecimento] = useState(FILTRO_ESTABELECIMENTO_DEFAULT);
+  const [filtroOcupacao, setFiltroOcupacao] = useState(FILTRO_OCUPACAO_DEFAULT);
 
   useEffect(() => {
     const getDados = async (municipioIdSus) => {
@@ -40,135 +37,42 @@ const ReducaoDeDanos = () => {
 
   const getPropsCardUltimoPeriodo = (acoes) => {
     const acaoTodosUltimoPeriodo = acoes
-      .find((acao) => acao.estabelecimento === "Todos" && acao.profissional_vinculo_ocupacao === "Todas" && acao.periodo === "Último período");
+      .find((acao) => acao.estabelecimento === 'Todos' && acao.profissional_vinculo_ocupacao === 'Todas' && acao.periodo === 'Último período');
 
     return {
       key: uuidv1(),
-      indicador: acaoTodosUltimoPeriodo["quantidade_registrada"],
-      titulo: `Total de ações de redução de danos em ${acaoTodosUltimoPeriodo["nome_mes"]}`,
-      indice: acaoTodosUltimoPeriodo["dif_quantidade_registrada_anterior"],
-      indiceDescricao: "últ. mês"
+      indicador: acaoTodosUltimoPeriodo['quantidade_registrada'],
+      titulo: `Total de ações de redução de danos em ${acaoTodosUltimoPeriodo['nome_mes']}`,
+      indice: acaoTodosUltimoPeriodo['dif_quantidade_registrada_anterior'],
+      indiceDescricao: 'últ. mês'
     };
   };
 
   const getPropsCardUltimos12Meses = (acoes) => {
     const acaoTodosUltimos12Meses = acoes
-      .find((acao) => acao.estabelecimento === "Todos" && acao.profissional_vinculo_ocupacao === "Todas");
+      .find((acao) => acao.estabelecimento === 'Todos' && acao.profissional_vinculo_ocupacao === 'Todas');
 
     return {
       key: uuidv1(),
-      indicador: acaoTodosUltimos12Meses["quantidade_registrada"],
-      titulo: `Total de ações de redução de danos entre ${acaoTodosUltimos12Meses["a_partir_do_mes"]} de ${acaoTodosUltimos12Meses["a_partir_do_ano"]} e ${acaoTodosUltimos12Meses["ate_mes"]} de ${acaoTodosUltimos12Meses["ate_ano"]}`,
-      indice: acaoTodosUltimos12Meses["dif_quantidade_registrada_anterior"],
-      indiceDescricao: "doze meses anteriores"
+      indicador: acaoTodosUltimos12Meses['quantidade_registrada'],
+      titulo: `Total de ações de redução de danos entre ${acaoTodosUltimos12Meses['a_partir_do_mes']} de ${acaoTodosUltimos12Meses['a_partir_do_ano']} e ${acaoTodosUltimos12Meses['ate_mes']} de ${acaoTodosUltimos12Meses['ate_ano']}`,
+      indice: acaoTodosUltimos12Meses['dif_quantidade_registrada_anterior'],
+      indiceDescricao: 'doze meses anteriores'
     };
-  };
-
-  const agregarPorEstabelecimentoEOcupacao = (acoes) => {
-    const acoesAgregadas = [];
-
-    acoes.forEach((acao) => {
-      const { estabelecimento, competencia, periodo, quantidade_registrada: quantidadeRegistrada, profissional_vinculo_ocupacao: ocupacao } = acao;
-      const acaoEncontrada = acoesAgregadas
-        .find((item) => item.estabelecimento === estabelecimento && item.ocupacao === ocupacao);
-
-      if (!acaoEncontrada) {
-        acoesAgregadas.push({
-          ocupacao,
-          estabelecimento,
-          quantidadesPorPeriodo: [{ competencia, periodo, quantidadeRegistrada }]
-        });
-      } else {
-        acaoEncontrada.quantidadesPorPeriodo.push({ competencia, periodo, quantidadeRegistrada });
-      }
-    });
-
-    return acoesAgregadas;
   };
 
   const ordenarQuantidadesPorCompetenciaAsc = (acoes) => {
-    return acoes.map(({ estabelecimento, ocupacao, quantidadesPorPeriodo }) => ({
-      ocupacao,
-      estabelecimento,
-      quantidadesPorPeriodo: quantidadesPorPeriodo
-        .sort((a, b) => new Date(a.competencia) - new Date(b.competencia))
-    }));
-  };
-
-  const getPropsFiltroEstabelecimento = (acoes) => {
-    const optionsSemDuplicadas = [];
-
-    acoes.forEach(({ estabelecimento }) => {
-      const acaoEncontrada = optionsSemDuplicadas
-        .find((item) => item.value === estabelecimento);
-
-      if (!acaoEncontrada) {
-        optionsSemDuplicadas.push({ value: estabelecimento, label: estabelecimento });
-      }
-    });
-
-    const optionPersonalizada = ({ children, ...props }) => (
-      <components.Control { ...props }>
-        Estabelecimento: { children }
-      </components.Control>
-    );
-
-    return {
-      options: optionsSemDuplicadas,
-      defaultValue: filtroEstabelecimento,
-      selectedValue: filtroEstabelecimento,
-      onChange: (selected) => setFiltroEstabelecimento({
-        value: selected.value,
-        label: selected.value
-      }),
-      isMulti: false,
-      isSearchable: false,
-      components: { Control: optionPersonalizada },
-      styles: { control: (css) => ({ ...css, paddingLeft: '15px' }) },
-    };
-  };
-
-  const getPropsFiltroOcupacao = (acoes) => {
-    const optionsSemDuplicadas = [];
-
-    acoes
-      .filter(({ estabelecimento }) => estabelecimento === filtroEstabelecimento.value)
-      .forEach(({ profissional_vinculo_ocupacao: ocupacao }) => {
-        const acaoEncontrada = optionsSemDuplicadas
-          .find((item) => item.value === ocupacao);
-
-        if (!acaoEncontrada) {
-          optionsSemDuplicadas.push({ value: ocupacao, label: ocupacao });
-        }
-      });
-
-    const optionPersonalizada = ({ children, ...props }) => (
-      <components.Control { ...props }>
-        CBO do profissional: { children }
-      </components.Control>
-    );
-
-    return {
-      options: optionsSemDuplicadas,
-      defaultValue: filtroOcupacao,
-      selectedValue: filtroOcupacao,
-      onChange: (selected) => setFiltroOcupacao({
-        value: selected.value,
-        label: selected.value
-      }),
-      isMulti: false,
-      isSearchable: false,
-      components: { Control: optionPersonalizada },
-      styles: { control: (css) => ({ ...css, paddingLeft: '15px' }) },
-    };
+    return acoes.sort((a, b) => new Date(a.competencia) - new Date(b.competencia));
   };
 
   const getOpcoesGraficoDeLinha = (acoes) => {
-    const acoesAgregadas = agregarPorEstabelecimentoEOcupacao(acoes);
-    const acoesOrdenadas = ordenarQuantidadesPorCompetenciaAsc(acoesAgregadas);
-    const acaoFiltrada = acoesOrdenadas.find(({ estabelecimento, ocupacao }) =>
+    const acoesFiltradas = acoes.filter(({
+      estabelecimento,
+      profissional_vinculo_ocupacao: ocupacao
+    }) =>
       estabelecimento === filtroEstabelecimento.value && ocupacao === filtroOcupacao.value
     );
+    const acoesOrdenadas = ordenarQuantidadesPorCompetenciaAsc(acoesFiltradas);
 
     return {
       tooltip: {
@@ -176,13 +80,6 @@ const ReducaoDeDanos = () => {
         axisPointer: {
           label: {
             backgroundColor: '#6a7985'
-          }
-        }
-      },
-      toolbox: {
-        feature: {
-          saveAsImage: {
-            title: "Salvar como imagem",
           }
         }
       },
@@ -194,19 +91,18 @@ const ReducaoDeDanos = () => {
       },
       xAxis: {
         type: 'category',
-        data: acaoFiltrada.quantidadesPorPeriodo.map(({ periodo }) => periodo)
+        data: acoesOrdenadas.map(({ periodo }) => periodo)
       },
       yAxis: {
         type: 'value'
       },
       series: [
         {
-          name: "Ações registradas",
-          data: acaoFiltrada.quantidadesPorPeriodo
-            .map(({ quantidadeRegistrada }) => quantidadeRegistrada),
+          name: 'Ações registradas',
+          data: acoesOrdenadas.map(({ quantidade_registrada: quantidadeRegistrada }) => quantidadeRegistrada),
           type: 'line',
           itemStyle: {
-            color: "#5367C9"
+            color: '#5367C9'
           },
         }
       ]
@@ -257,17 +153,25 @@ const ReducaoDeDanos = () => {
       { acoes.length !== 0 &&
         <>
           <div className={ styles.Filtros }>
-            <div className={ styles.Filtro }>
-              <Select { ...getPropsFiltroEstabelecimento(acoes) } />
-            </div>
-            <div className={ styles.Filtro }>
-              <Select { ...getPropsFiltroOcupacao(acoes) } />
-            </div>
+            <FiltroTexto
+              dados={ acoes }
+              label='Estabelecimento'
+              propriedade='estabelecimento'
+              valor={ filtroEstabelecimento }
+              setValor={ setFiltroEstabelecimento }
+            />
+            <FiltroTexto
+              dados={ acoes }
+              label='CBO do profissional'
+              propriedade='profissional_vinculo_ocupacao'
+              valor={ filtroOcupacao }
+              setValor={ setFiltroOcupacao }
+            />
           </div>
 
           <ReactEcharts
             option={ getOpcoesGraficoDeLinha(acoes) }
-            style={ { width: "100%", height: "70vh" } }
+            style={ { width: '100%', height: '70vh' } }
           />
         </>
       }
