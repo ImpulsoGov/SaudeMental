8d7520c62b5af127525b187c9f2cd510c38597a3
Taina Santana <tainnaps@gmail.com>
2023-09-29 12:09:19 -0300

    Adiciona componente GraficoCondicaoUsuarios

--- components/Graficos/index.js
+++ components/Graficos/index.js
@@ -1,7 +1,9 @@
-import GraficoGeneroPorFaixaEtaria from './GeneroPorFaixaEtaria';
+import GraficoCondicaoUsuarios from './CondicaoUsuarios';
 import GraficoDonut from './Donut';
+import GraficoGeneroPorFaixaEtaria from './GeneroPorFaixaEtaria';
 
 export {
-  GraficoGeneroPorFaixaEtaria,
-  GraficoDonut
+  GraficoCondicaoUsuarios,
+  GraficoDonut,
+  GraficoGeneroPorFaixaEtaria
 };
