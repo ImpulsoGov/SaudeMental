a941946068bb171f3d243431bf64d00919ec04d8
Taina Santana <tainnaps@gmail.com>
2023-10-03 15:29:00 -0300

    Adicona GraficoProcedimentosPorTempoServico

--- components/Graficos/index.js
+++ components/Graficos/index.js
@@ -2,10 +2,12 @@ import GraficoCondicaoUsuarios from './CondicaoUsuarios';
 import GraficoDonut from './Donut';
 import GraficoGeneroPorFaixaEtaria from './GeneroPorFaixaEtaria';
 import GraficoEncaminhamentos from './Encaminhamentos';
+import GraficoProcedimentosPorTempoServico from './ProcedimentosPorTempoServico';
 
 export {
   GraficoCondicaoUsuarios,
   GraficoDonut,
   GraficoGeneroPorFaixaEtaria,
   GraficoEncaminhamentos,
+  GraficoProcedimentosPorTempoServico
 };
