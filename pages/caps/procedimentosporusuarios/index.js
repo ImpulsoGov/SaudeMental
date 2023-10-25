6962d2c9dd565445ad5223045e055bebd73dea65
Taina Santana <tainnaps@gmail.com>
2023-10-24 23:11:15 -0300

    Atualiza filtragem do gráfico de procedimentos

--- pages/caps/procedimentosporusuarios/index.js
+++ pages/caps/procedimentosporusuarios/index.js
@@ -48,38 +48,19 @@ const ProcedimentosPorUsuarios = () => {
     }
   }, []);
 
-  // Controla mudanças a cada interação com filtros de estabelecimento/períodos
-  // do gráfico de procedimentos por tempo de serviço
   useEffect(() => {
     if (session?.user.municipio_id_ibge) {
       setLoadingProcedimentosPorTempoServico(true);
-      // Cria um array de promises que, quando resolvidas, trarão os resultados das
-      // requisições feitas à API para cada período de estabelecimento selecionados.
-      // Essa abordagem foi adota para que as requisições feitas fossem o mais atômicas
-      // possíveis para facilitar a reutilização do cache pelo navegador, ex: se
-      // fizéssemos requisições passando todos os períodos selecionados numa mesma requisição,
-      // quando o usuário selecionasse um novo período, seria feita uma nova requisição diferente
-      // anterior:
-      // 1ª requisição -> estabelecimento=Todos, periodos=Último período
-      // Usuário adiciona Jun/23 no filtro múltiplo
-      // 2ª requisição -> estabelecimento=Todos, periodos=Último período-Jun/23
-      // Como a 2ª requisição é diferente da 1ª, a API é chamada novamente.
-      // Da forma feita aqui, é feita uma requisição pra cada período selecio do estabelecimento:
-      // 1ª requisição -> estabelecimento=Todos, periodos=Último período
-      // Usuário adiciona Jun/23 no filtro múltiplo
-      // Próximas requisições -> estabelecimento=Todos, periodos=Último período e estabelecimento=Todos, periodos=Jun/23
-      // Como a requisição estabelecimento=Todos, periodos=Último período já foi feita antes, os dados em cache são usados e a API não é chamada para esse endpoint
+
       const promises = filtroPeriodoProcedimento.map(({ value: periodo }) => {
-        return obterProcedimentosPorTempoServico(
-          session?.user.municipio_id_ibge,
-          filtroEstabelecimentoProcedimento.value,
-          periodo
-        );
+        return obterProcedimentosPorTempoServico({
+          municipioIdSus: session?.user.municipio_id_ibge,
+          estabelecimentos: filtroEstabelecimentoProcedimento.value,
+          periodos: periodo
+        });
       });
-      // Resolve todas as promises das requisições (a ordem de resolução não importa)
-      // A variável "respostas" é um array de arrays
+
       Promise.all(promises).then((respostas) => {
-        // Transforma o array de arrays num só array com os dados de todas as requisições
         const respostasUnificadas = [].concat(...respostas);
         setProcedimentosPorTempoServico(respostasUnificadas);
       });
@@ -329,40 +310,34 @@ const ProcedimentosPorUsuarios = () => {
         fonte="Fonte: BPA-i e RAAS/SIASUS - Elaboração Impulso Gov"
       />
 
-      { procedimentosPorTempoServico.length !== 0
-        ? (
-          <>
-            <div className={ styles.Filtros }>
-              <FiltroTexto
-                width={ '50%' }
-                dados={ estabelecimentos }
-                valor={ filtroEstabelecimentoProcedimento }
-                setValor={ setFiltroEstabelecimentoProcedimento }
-                label={ 'Estabelecimento' }
-                propriedade={ 'estabelecimento' }
-              />
-              <FiltroCompetencia
-                width={ '50%' }
-                dados={ periodos }
-                valor={ filtroPeriodoProcedimento }
-                setValor={ setFiltroPeriodoProcedimento }
-                isMulti
-                label={ 'Competência' }
-              />
-            </div>
+      <div className={ styles.Filtros }>
+        <FiltroTexto
+          width={ '50%' }
+          dados={ estabelecimentos }
+          valor={ filtroEstabelecimentoProcedimento }
+          setValor={ setFiltroEstabelecimentoProcedimento }
+          label={ 'Estabelecimento' }
+          propriedade={ 'estabelecimento' }
+        />
 
-            { loadingProcedimentosPorTempoServico
-              ? <Spinner theme="ColorSM" height="70vh" />
-              : <ReactEcharts
-                option={ getOpcoesGraficoProcedimentoPorTempo(
-                  procedimentosPorTempoServico
-                ) }
-                style={ { width: "100%", height: "70vh" } }
-              />
-            }
-          </>
-        )
-        : <Spinner theme="ColorSM" />
+        <FiltroCompetencia
+          width={ '50%' }
+          dados={ periodos }
+          valor={ filtroPeriodoProcedimento }
+          setValor={ setFiltroPeriodoProcedimento }
+          isMulti
+          label={ 'Competência' }
+        />
+      </div>
+
+      { loadingProcedimentosPorTempoServico
+        ? <Spinner theme="ColorSM" />
+        : <ReactEcharts
+          option={ getOpcoesGraficoProcedimentoPorTempo(
+            procedimentosPorTempoServico
+          ) }
+          style={ { width: "100%", height: "70vh" } }
+        />
       }
     </div>
   );
