63b58c553b989cd2035d5c65826fcca7b9695a12
Taina Santana <tainnaps@gmail.com>
2023-11-22 13:31:46 -0300

    Revert "Altera key cards ambulatório último mês"
    This reverts commit d475c1eade3db392a6d7faea12e7a1162713683b.

--- components/CardsAmbulatorio/CardsAmbulatorioUltimoMes.jsx
+++ components/CardsAmbulatorio/CardsAmbulatorioUltimoMes.jsx
@@ -13,7 +13,7 @@ const CardsAmbulatorioUltimoMes = ({ atendimento }) => {
               indicador={atendimento.procedimentos_realizados}
               indice={atendimento.dif_procedimentos_realizados_anterior}
               indiceDescricao='ult. mês'
-              key={`${atendimento.id}${atendimento.procedimentos_realizados}`}
+              key={`${atendimento.procedimentos_realizados}${atendimento.dif_procedimentos_realizados_anterior}`}
             />,
             <CardInfoTipoA
               titulo={`Total de atendimentos por hora trabalhada em ${atendimento.nome_mes}`}
@@ -21,7 +21,7 @@ const CardsAmbulatorioUltimoMes = ({ atendimento }) => {
               indice={atendimento.dif_procedimentos_por_hora_anterior}
               indiceDescricao='ult. mês'
               tooltip={'Indicador é calculado a partir da divisão do total de atendimentos registrados pelo total de horas de trabalho dos profissionais estabelecidas em contrato. De tal modo, valores podem apresentar subnotificação em caso de férias, licenças, feriados, números de maior número de finais de semana no mês.'}
-              key={`${atendimento.id}${atendimento.procedimentos_por_hora}`}
+              key={`${atendimento.procedimentos_por_hora}${atendimento.dif_procedimentos_por_hora_anterior}`}
             />
           ]}
         />
