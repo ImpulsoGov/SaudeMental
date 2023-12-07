e6c19f9c91ba4ad4b4b6c15628dd1eae97513d29
Gabrielle Arruda <gabriellearrudac@gmail.com>
2023-12-07 12:03:20 -0300

    Update API_URL.js

--- constants/API_URL.js
+++ constants/API_URL.js
@@ -1,3 +1,3 @@
-export const API_SAUDE_MENTAL_URL = 'https://api-sm.impulsogov.org/';
+export const API_SAUDE_MENTAL_URL = 'https://impulsoapi.herokuapp.com/;
 
 export const API_USUARIOS_URL = 'https://impulsoapi.herokuapp.com/';
