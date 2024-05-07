9d9d02784d8e54101e3a45830ccfb69c6d20c3fc
João Vittor Souza <jvns2010@gmail.com>
2024-05-07 14:41:08 -0300

    aponta url para api de staging

--- constants/API_URL.js
+++ constants/API_URL.js
@@ -1,3 +1,3 @@
-export const API_SAUDE_MENTAL_URL = 'https://api-sm.impulsogov.org/';
+export const API_SAUDE_MENTAL_URL = 'https://api-sm-staging-6cd5113930fa.herokuapp.com/';
 
 export const API_USUARIOS_URL = 'https://api-usuarios-sm-70d45d3b826a.herokuapp.com/';
