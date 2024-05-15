a0f440b72ff015c001eff2e434fc6f4228db971d
João Vittor Souza <jvns2010@gmail.com>
2024-05-15 14:36:56 -0300

    corrige estilizacao texto cards zerados

--- pages/caps/producao/index.js
+++ pages/caps/producao/index.js
@@ -204,7 +204,8 @@ const Producao = () => {
           posicao: null,
           url: ''
         } }
-        texto="Alguns indicadores podem não estar disponíveis nessa página por falta de dados necessários para exibi-los. Verifique se existem problemas de registro. <br><br> Em caso de dúvidas, entre em contato via nosso <a href = 'https://saudemental.impulsogov.org/duvidas'>  formulário de suporte </a>, <a href='https://wa.me/5511942642429'> whatsapp </a> ou e-mail(saudemental@impulsogov.org)."
+        texto={`<p style="font-size: 16px;">Alguns indicadores podem não estar disponíveis nessa página por falta de dados necessários para exibi-los. Verifique se existem problemas de registro.</p>
+        <p style="font-size: 16px;">Em caso de dúvidas, entre em contato via nosso <a href="https://saudemental.impulsogov.org/duvidas">formulário de suporte</a>, <a href="https://wa.me/5511942642429">whatsapp</a> ou e-mail (<a href="mailto:saudemental@impulsogov.org">saudemental@impulsogov.org</a>).</p>`}
         botao={ {
           label: '',
           url: ''
