e7a6a0e75623a06ff2988590b2e4d25ac3677e15
João Vittor Souza <jvns2010@gmail.com>
2024-06-03 17:10:14 -0300

    muda consumo de ultima competencia disponivel e add verificacao em cards

--- pages/caps/producao/index.js
+++ pages/caps/producao/index.js
@@ -11,7 +11,7 @@ import { redirectHomeNotLooged } from '../../../helpers/RedirectHome';
 import { getEstabelecimentos, getPeriodos, obterNomesDeProcedimentosPorTipo, obterProcedimentosPorHora, obterProcedimentosPorTipo } from '../../../requests/caps';
 import { ordenarCrescentePorPropriedadeDeTexto } from '../../../utils/ordenacao';
 import styles from '../Caps.module.css';
-
+import { getTextoCardsZerados } from '../../../utils/getTextoCardsZerados';
 const OCUPACOES_NAO_ACEITAS = ['Todas', null];
 
 export function getServerSideProps(ctx) {
@@ -197,29 +197,11 @@ const Producao = () => {
 
     return procedimentosAgregados;
   };
-  const getTextoCardsZerados = () => {
-    return (
-      <TituloSmallTexto
-        imagem={ {
-          posicao: null,
-          url: ''
-        } }
-        texto={`<p style="font-size: 16px;">Alguns indicadores podem não estar disponíveis nessa página por falta de dados necessários para exibi-los. Verifique se existem problemas de registro.</p>
-        <p style="font-size: 16px;">Em caso de dúvidas, entre em contato via nosso <a href="https://saudemental.impulsogov.org/duvidas">formulário de suporte</a>, <a href="https://wa.me/5511942642429">whatsapp</a> ou e-mail (<a href="mailto:saudemental@impulsogov.org">saudemental@impulsogov.org</a>).</p>`}
-        botao={ {
-          label: '',
-          url: ''
-        } }
-        titulo=""
-      />
-    );
-  };
   const verificaCardsZerados = (procedimentos) => {
-    procedimentos.forEach((procedimento) => {
-      if (procedimento.procedimentos_por_hora !== null && procedimento.procedimentos_por_hora !== 0) {
-        return true;
-      }
-    });
+    return procedimentos.some((procedimento =>
+      procedimento.procedimentos_por_hora !== null &&
+      procedimento.procedimentos_por_hora !== undefined
+    ));
   };
   const getCardsProcedimentosHoraPorEstabelecimento = (procedimentos) => {
     const procedimentosFiltrados = procedimentos
